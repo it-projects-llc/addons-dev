23cf7a043f98a6e7ff3f3354a6517b29a4dc8013
Dinar <gabbasov@it-projects.info>
2015-12-22 23:21:17 +0500

    step 1
    create module

---
+++ website_page_currencies/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website page currencies",
+    'summary': """Web page currencies""",
+    'description': """Web page currencies""",
+    'author': "IT-Projects LLC, Dinar Gabbasov",
+    'website': "https://twitter.com/gabbasov_dinar",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['website', 'base'],
+    'data': [
+        'views/website_page_currencies_views.xml',
+        'views/website_page_currencies_templates.xml',
+    ],
+}
