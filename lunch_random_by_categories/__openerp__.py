84719f8c5ede234cc9cdc77e357140d0998b595c
Dinar <gabbasov@it-projects.info>
2015-12-23 15:14:16 +0500

    step 1
    create new module for Lunch

---
+++ lunch_random_by_categories/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lunch random by categories",
+    'summary': """Random by categories for apllication Lunch""",
+    'description': """Random by categories for apllication Lunch""",
+    'author': "IT-Projects LLC, Dinar Gabbasov",
+    'website': "https://twitter.com/gabbasov_dinar",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['lunch'],
+    'data': [
+        'views/lunch_random_by_categories_views.xml',
+        'views/lunch_random_by_categories_templates.xml',
+    ],
+}
