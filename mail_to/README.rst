bd098656c0565552ce3503c4ed7a276455e1017c
x620 <romanchenko@it-projects.info>
2016-04-25 11:18:20 +0500

    [ADD] Init commit for mail_to module: create __openerp__.py and README.rst

---
+++ mail_to/README.rst
@@ -0,0 +1,11 @@
+======================================
+ Label on the message about recipient
+======================================
+
+Add recipient label on the right of message in the Inbox channel.
+
+Further information
+-------------------
+HTML Description: https://apps.odoo.com/apps/modules/9.0/mail_to/
+
+Tested on Odoo 9.0 d3dd4161ad0598ebaa659fbd083457c77aa9448d
---
+++ mail_to/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Label on the message about recipient""",
+    "summary": """Add recipient label on the right of message in the Inbox channel""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
