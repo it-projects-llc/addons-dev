29be82eafb49925c19d122832e3943ca368f18de
x620 <romanchenko@it-projects.info>
2016-04-25 15:21:18 +0500

    [ADD] Init commit for mail_reply module: create changelog.rst, index.rst, README.rst, __openerp__.py

---
+++ mail_reply/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Show reply button""",
+    "summary": """The module adds the reply button on the message is not attached to the recording""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
