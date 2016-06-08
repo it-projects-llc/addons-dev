44a2f1b671ffaedf64ba3d19ce266028fb0a13fc
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-08 22:20:46 +0500

    [ADD] fleet_booking: fleet_booking.document model. top-level menu item for rental documents

--- fleet_booking/models/rental.py
+++ fleet_booking/models/rental.py
@@ -2,17 +2,41 @@
 import openerp
 from openerp import models, fields, api
 
-
-class FleetBookingRental(models.Model):
-    _name = 'fleet_booking.rental'
+class FleetBookingDocument(models.Model):
+    _name = 'fleet_booking.document'
 
     name = fields.Char(string='Document Reference', required=True, copy=False, readonly=True, index=True, default='New')
 
     state = fields.Selection([
         ('draft', 'Quotation'),
         ('booked', 'Booked'),
-        ('sale', 'Sale Order'),
-        ('done', 'Done'),
+        ('confirmed', 'Confirmed'),
+        ('extended', 'Extended'),
         ('cancel', 'Cancelled'),
-        ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='draft')
- 
+        ('return_draft', 'Return Draft'),
+        ('return_open', 'Return Open'),
+        ('return_closed', 'Return Closed'),
+        ], string='Status', readonly=True, copy=False, index=True, default='draft')
+
+    type = fields.Selection([
+            ('rent','Rent'),
+            ('extended_rent','Extended Rent'),
+            ('return','Return'),
+        ], readonly=True, index=True, change_default=True)
+
+
+    origin = fields.Char(string='Source Document',
+        help="Reference of the document that produced this document.",
+        readonly=True, states={'draft': [('readonly', False)]})
+
+
+class FleetBookingRentDocument(models.Model):
+    _name = 'fleet_booking.rent_document'
+
+    _inherits = {
+                 'fleet_booking.document': 'document_id',
+                 }
+
+    document_id = fields.Many2one('fleet_booking', required=True,
+            string='Related Document', ondelete='restrict',
+            help='common part of all three types of the documents', auto_join=True)
