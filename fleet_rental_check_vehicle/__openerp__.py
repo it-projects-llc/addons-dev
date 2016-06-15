0080144b45a832607d5983438f0e022345be74ea
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-15 19:08:47 +0500

    [ADD] fleet_rental_check_vehicle: add ir.model.access security

--- fleet_rental_check_vehicle/__openerp__.py
+++ fleet_rental_check_vehicle/__openerp__.py
@@ -18,6 +18,7 @@
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
+        'security/ir.model.access.csv',
         "views/fleet_rental_check_vehicle.xml",
         "views/fleet_rental_document.xml",
     ],
