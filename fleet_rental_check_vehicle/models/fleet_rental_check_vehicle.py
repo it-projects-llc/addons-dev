ab3c640caf3c60639bc4b2879c2cbb250062bace
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-16 13:31:29 +0500

    [FIX] fleet_rental_check_vehicle: make item_id from check_line readonly as it is generated by default

--- fleet_rental_check_vehicle/models/fleet_rental_check_vehicle.py
+++ fleet_rental_check_vehicle/models/fleet_rental_check_vehicle.py
@@ -12,7 +12,7 @@ class FleetRentalItemToCheck(models.Model):
 class FleetRentalCheckLine(models.Model):
     _name = 'fleet_rental.check_line'
 
-    item_id = fields.Many2one('fleet_rental.item_to_check', string='Item', ondelete='restrict', required=True)
+    item_id = fields.Many2one('fleet_rental.item_to_check', string='Item', ondelete='restrict', required=True, readonly=True)
     document_id = fields.Many2one('fleet_rental.document', string='Document', ondelete='cascade', required=True)
     check_yes = fields.Boolean(string='yes', default=False)
     check_no = fields.Boolean(string='no', default=False)
