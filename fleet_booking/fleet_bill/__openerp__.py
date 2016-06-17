0fce25b508ed06ec677b443856535ac2b617b85e
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-06-17 11:44:27 +0500

    [ADD] fleet bill

---
+++ fleet_booking/fleet_bill/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Fleet costs bills""",
+    "summary": """Creates bills for generated costs""",
+    "category": "fleet",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC",
+    "website": "https://it-projects.info",
+    "license": "GPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+}
