b36fa38ad65cfbca703ac8d92b96ece258fc9303
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-17 13:34:30 +0500

    [FIX] fleet_rental_document_rent: there was wrong name of base model

--- fleet_rental_document_rent/models/fleet_rental_document_rent.py
+++ fleet_rental_document_rent/models/fleet_rental_document_rent.py
@@ -19,6 +19,6 @@ class FleetBookingDocumentRent(models.Model):
                  'fleet_booking.document': 'document_id',
                  }
 
-    document_id = fields.Many2one('fleet_booking.document', required=True,
+    document_id = fields.Many2one('fleet_rental.document', required=True,
             string='Related Document', ondelete='restrict',
             help='common part of all three types of the documents', auto_join=True)
