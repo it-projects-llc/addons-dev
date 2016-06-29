39a6f98b3dab34a03675d638fb8904c138615f2b
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-29 17:44:19 +0500

    [ADD] fleet_rental_document: add svg_vehicle_part model and data and svg image of vehicle

--- fleet_rental_document/models/fleet_rental_check_vehicle.py
+++ fleet_rental_document/models/fleet_rental_check_vehicle.py
@@ -31,3 +31,11 @@ class FleetRentalCheckLine(models.Model):
         if vals.get('return_check_no'):
             vals.update({'return_check_yes': False})
         return super(FleetRentalCheckLine, self).write(vals)
+
+
+class FleetRentalSVGVehiclePart(models.Model):
+    _name = 'fleet_rental.svg_vehicle_part'
+
+    name = fields.Char(string='Part name')
+    path_ID = fields.Char(string='svg path id', required=True)
+
