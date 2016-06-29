c060b3ff886f7917de218b981835156aa320157e
ilyasProgrammer <rakhimkulov@it-projects.info>
2016-06-29 10:42:08 +0500

    [IMP] #rental extend

--- fleet_rental_document/models/fleet_rental_document_rent.py
+++ fleet_rental_document/models/fleet_rental_document_rent.py
@@ -24,6 +24,7 @@ class FleetRentalDocumentRent(models.Model):
             help='common part of all three types of the documents', auto_join=True)
 
     document_return_ids = fields.One2many('fleet_rental.document_return', 'document_rent_id', string='Return documents', copy=False)
+    document_extend_ids = fields.One2many('fleet_rental.document_extend', 'document_rent_id', string='Extend documents', copy=False)
     user_branch_id = fields.Many2one('fleet_branch.branch', default=lambda self: self.env.user.branch_id.id)
 
     @api.multi
@@ -62,6 +63,8 @@ class FleetRentalDocumentRent(models.Model):
                'document_rent_id': rent.id,
                'origin': rent.name,
                'exit_datetime': rent.exit_datetime,
+               'type': 'return',
+               'return_datetime': fields.Datetime.now(),
                })
             for r in self.check_line_ids:
                 for w in document_return.check_line_ids:
@@ -71,6 +74,32 @@ class FleetRentalDocumentRent(models.Model):
                         break
 
         return self.action_view_document_return()
+
+    @api.multi
+    def action_create_extend(self):
+        document_rent_obj = self.env['fleet_rental.document_extend']
+        for rent in self:
+            rent.state = 'extended'
+            self.vehicle_id.state_id = self.env.ref('fleet.vehicle_state_active')
+            document_extend = document_rent_obj.create({
+               'partner_id': rent.partner_id.id,
+               'vehicle_id': rent.vehicle_id.id,
+               'allowed_kilometer_per_day': rent.allowed_kilometer_per_day,
+               'rate_per_extra_km': rent.rate_per_extra_km,
+               'daily_rental_price': rent.daily_rental_price,
+               'document_rent_id': rent.id,
+               'origin': rent.name,
+               'exit_datetime': rent.return_datetime,
+               'type': 'extended_rent',
+               })
+            for r in self.check_line_ids:
+                for w in document_extend.check_line_ids:
+                    if r.item_id == w.item_id:
+                        w.exit_check_yes = r.exit_check_yes
+                        w.exit_check_no = r.exit_check_no
+                        break
+
+        return self.action_view_document_extend()
 
     @api.multi
     def action_view_document_return(self):
@@ -94,9 +123,58 @@ class FleetRentalDocumentRent(models.Model):
             result = {'type': 'ir.actions.act_window_close'}
         return result
 
+    @api.multi
+    def action_view_document_extend(self):
+        document_extend_ids = self.mapped('document_extend_ids')[0]
+        action = self.env.ref('fleet_rental_document.fleet_rental_document_extend_draft_act')
+        form_view_id = self.env.ref('fleet_rental_document.fleet_rental_document_extend_form').id
+
+        result = {
+            'name': action.name,
+            'help': action.help,
+            'type': action.type,
+            'views': [(form_view_id, 'form')],
+            'target': action.target,
+            'context': action.context,
+            'res_model': action.res_model,
+        }
+        if len(document_extend_ids) == 1:
+            # TODO: think about what if there would be more than one return documents
+            result['res_id'] = document_extend_ids.ids[0]
+        else:
+            result = {'type': 'ir.actions.act_window_close'}
+        return result
+
     @api.model
     def create(self, vals):
         if vals.get('name', 'New') == 'New':
             vals['name'] = self.env['ir.sequence'].next_by_code('fleet_rental.document_rent') or 'New'
         result = super(FleetRentalDocumentRent, self).create(vals)
         return result
+
+
+class FleetRentalDocumentExtend(models.Model):
+    _name = 'fleet_rental.document_extend'
+    _inherit = 'fleet_rental.document_rent'
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('booked', 'Booked'),
+        ('confirmed', 'Confirmed'),
+        ('cancel', 'Cancelled'),
+        ], string='Status', readonly=True, copy=False, index=True, default='draft')
+
+    document_rent_id = fields.Many2one('fleet_rental.document_rent', required=True,
+                                       string='Related Rent Document', ondelete='restrict',
+                                       help='Source Rent document')
+
+
+    @api.multi
+    def action_submit(self):
+        for rent in self:
+            rent.state = 'booked'
+            self.vehicle_id.state_id = self.env.ref('fleet.vehicle_state_booked')
+
+    @api.multi
+    def action_view_invoice(self):
+        return self.mapped('document_id').action_view_invoice()
