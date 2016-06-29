cb3334de2566519fd255461f93eaa5cf61159a09
Ildar Nasyrov <nasyrov@it-projects.info>
2016-06-29 18:12:02 +0500

    [REF] fleet_rental_document: attache parts and svg to document instead of vehicle

--- fleet_rental_document/models/fleet_rental_document.py
+++ fleet_rental_document/models/fleet_rental_document.py
@@ -50,6 +50,7 @@ class FleetRentalDocument(models.Model):
     balance = fields.Float(string='Balance', compute="_compute_balance", store=True, digits_compute=dp.get_precision('Product Price'), readonly=True)
 
     check_line_ids = fields.One2many('fleet_rental.check_line', 'document_id', string='Vehicle rental check lines')
+    part_line_ids = fields.One2many('fleet_rental.svg_vehicle_part_line', 'document_id', string='Vehicle part')
 
     invoice_ids = fields.Many2many("account.invoice", string='Invoices', compute="_get_invoiced", readonly=True, copy=False)
 
@@ -67,13 +68,13 @@ class FleetRentalDocument(models.Model):
     @api.multi
     def _compute_png(self):
         for rec in self:
-            f = open('/'.join([os.path.dirname(os.path.realpath(__file__+ '//..//..')),  # TODO needs better decision
-                               'fleet_vehicle_svg/static/src/img/car-cutout.svg']), 'r')
+            f = open('/'.join([os.path.dirname(os.path.realpath(__file__)),
+                               'static/src/img/car-cutout.svg']), 'r')
             svg_file = f.read()
             dom = etree.fromstring(svg_file)
-            for part in rec.part_ids:
-                if part.state == 'broken':
-                    for el in dom.xpath('//*[@id="%s"]' % part.part_id):
+            for line in rec.part_line_ids:
+                if line.state == 'broken':
+                    for el in dom.xpath('//*[@id="%s"]' % line.part_id.path_ID):
                         el.attrib['fill'] = 'red'
             f.close()
             with Image(blob=etree.tostring(dom), format='svg') as img:
@@ -122,11 +123,11 @@ class FleetRentalDocument(models.Model):
     @api.model
     def default_get(self, fields_list):
         result = super(FleetRentalDocument, self).default_get(fields_list)
-        document_id= self._context.get('active_id', False)
         items = self.env['fleet_rental.item_to_check'].search([])
-        check_line_obj = self.env['fleet_rental.check_line']
+        parts = self.env['fleet_rental.svg_vehicle_part'].search([])
 
         result['check_line_ids'] = [(5, 0, 0)] + [(0, 0, {'item_id': item.id,'exit_check_yes': False, 'exit_check_no': False,'exit_check_yes': False, 'exit_check_no': False,}) for item in items]
+        result['part_line_ids'] = [(5, 0, 0)] + [(0, 0, {'part_id': part.id}) for part in parts]
 
         return result
 
