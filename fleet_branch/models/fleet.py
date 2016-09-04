726f56fae06d66976ada81dc64c92e448e1df95d
Ildar Nasyrov <nasyrov@it-projects.info>
2016-09-04 11:01:52 +0500

    [ADD] fleet_branch: mail message subtypes

--- fleet_branch/models/fleet.py
+++ fleet_branch/models/fleet.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from openerp import fields
 from openerp import models
+from openerp import api
 
 
 class Vehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
     branch_id = fields.Many2one('fleet_branch.branch', string='Current branch')
+    color_id = fields.Many2one(track_visibility='onchange')
+
+    @api.multi
+    def _track_subtype(self, init_values):
+        self.ensure_one()
+        if 'color_id' in init_values:
+            return 'fleet_branch.mt_test_branch'
+        return super(Vehicle, self)._track_subtype(init_values)
