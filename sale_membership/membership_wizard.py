5b367e5f1fda70e07c3e3c80d104e3eb7b786fbf
Ildar Nasyrov <nasyrov@it-projects.info>
2016-09-22 12:18:00 +0500

    [ADD] sale_membership: add the ability to unblock blocked customers

--- sale_membership/membership_wizard.py
+++ sale_membership/membership_wizard.py
@@ -13,6 +13,7 @@ class MembershipWizard(models.TransientModel):
     action_type = fields.Selection([
         ('block', 'Block'),
         ('demote', 'Demote'),
+        ('unblock', 'Unblock'),
     ])
 
     demoting_reason = fields.Text(string='Demoting reason')
@@ -47,5 +48,12 @@ class MembershipWizard(models.TransientModel):
             self.partner_id.write({'blocked': True})
             self.env['sale_membership.log'].create({'partner_id': self.partner_id.id,
                                                     'blocked': True,
+                                                    'reason': self.blocking_reason,
+                                                    })
+
+        elif self.action_type == 'unblock':
+            self.partner_id.write({'blocked': False})
+            self.env['sale_membership.log'].create({'partner_id': self.partner_id.id,
+                                                    'blocked': False,
                                                     'reason': self.blocking_reason,
                                                     })
