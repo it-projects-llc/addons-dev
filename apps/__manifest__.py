b8022f5cd767752aaf049ee8c22d25e75adbff1d
ilmir <ilmir.karamov@gmail.com>
2016-11-17 13:08:55 +0500

    [DOC] apps module

--- apps/__manifest__.py
+++ apps/__manifest__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Apps',
+    'summary': """ """,
+    'images': [],
     'version': '1.0.0',
+    'application': False,
     'author': 'IT-Projects LLC, Dinar Gabbasov',
+    'website': "https://it-projects.info",
     'category': 'Access',
-    'website': 'https://twitter.com/gabbasov_dinar',
     'license': 'GPL-3',
+
     'depends': [
         'access_apps',
     ],
@@ -13,5 +17,11 @@
         'views/apps_view.xml',
         'security.xml',
     ],
+
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+
+    'auto_install': False,
     'installable': True
 }
