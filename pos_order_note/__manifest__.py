6e29b6c3cd6eb96e796993dc9f553d4c7e2839a6
GabbasovDinar <gabbasov@it-projects.info>
2017-03-10 12:52:54 +0500

    [ADD] Add security file

--- pos_order_note/__manifest__.py
+++ pos_order_note/__manifest__.py
@@ -19,6 +19,7 @@
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
+        "security/ir.model.access.csv",
         "views/views.xml",
         "views/template.xml",
     ],
