d4b5673317d4e6f16aad2d6b05b296e5ddb944be
GabbasovDinar <gabbasov@it-projects.info>
2017-03-21 11:24:31 +0500

    [ADD] Added security file

--- pos_order_cancel/__manifest__.py
+++ pos_order_cancel/__manifest__.py
@@ -19,6 +19,7 @@
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
+        "security/ir.model.access.csv",
         "views/template.xml",
         "views/views.xml",
     ],
