770c8338e68efb974efbeec317c32eecc56bc038
GabbasovDinar <gabbasov@it-projects.info>
2017-03-31 13:54:57 +0500

    [FIX] Fixes travis errors

--- web_preview/controllers/controller.py
+++ web_preview/controllers/controller.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
 import werkzeug.utils
@@ -9,14 +10,14 @@ from odoo.addons.web.controllers.main import binary_content
 
 class Controller(Binary):
     @http.route(['/web/pdf',
-        '/web/pdf/<string:xmlid>',
-        '/web/pdf/<string:xmlid>/<string:filename>',
-        '/web/pdf/<int:id>',
-        '/web/pdf/<int:id>/<string:filename>',
-        '/web/pdf/<int:id>-<string:unique>',
-        '/web/pdf/<int:id>-<string:unique>/<string:filename>',
-        '/web/pdf/<string:model>/<int:id>/<string:field>',
-        '/web/pdf/<string:model>/<int:id>/<string:field>/<string:filename>'], type='http', auth="public")
+                 '/web/pdf/<string:xmlid>',
+                 '/web/pdf/<string:xmlid>/<string:filename>',
+                 '/web/pdf/<int:id>',
+                 '/web/pdf/<int:id>/<string:filename>',
+                 '/web/pdf/<int:id>-<string:unique>',
+                 '/web/pdf/<int:id>-<string:unique>/<string:filename>',
+                 '/web/pdf/<string:model>/<int:id>/<string:field>',
+                 '/web/pdf/<string:model>/<int:id>/<string:field>/<string:filename>'], type='http', auth="public")
     def content_pdf(self, xmlid=None, model='ir.attachment', id=None, field='datas', filename_field='datas_fname', unique=None, filename=None, mimetype=None, download=False):
         status, headers, content = binary_content(xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename, filename_field=filename_field, download=download, mimetype=mimetype, default_mimetype='application/pdf')
         if status == 304:
