8274965e92f1ab3bb3ea0406e9edf97695b1d752
GabbasovDinar <gabbasov@it-projects.info>
2017-04-18 14:20:49 +0500

    [FIX] Fixes lint error

--- pos_printer_network/static/src/js/network_printer.js
+++ pos_printer_network/static/src/js/network_printer.js
@@ -23,8 +23,7 @@ odoo.define('pos_restaurant.network_printer', function (require) {
                 if (printer_obj.network_printer) {
                     item.config.network_printer = printer_obj.network_printer;
                     self.ready.then(function () {
-                        var parent;
-                        item.connection = new Session(parent, self.proxy.host, { use_cors: true});
+                        item.connection = new Session(void(0), self.proxy.host, { use_cors: true});
                     });
                 }
             });
@@ -62,8 +61,7 @@ odoo.define('pos_restaurant.network_printer', function (require) {
     devices.ProxyDevice.include({
         message : function(name,params){
             if (name === 'print_xml_receipt' && this.pos.config.receipt_network_printer_ip) {
-                var parent;
-                var connection = new Session(parent, this.pos.proxy.host, {
+                var connection = new Session(void(0), this.pos.proxy.host, {
                     use_cors: true
                 });
                 var callbacks = this.notifications[name] || [];
