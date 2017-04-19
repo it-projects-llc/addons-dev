c60721e7bb0bac21c0a53324a6d1f701dc83701f
GabbasovDinar <gabbasov@it-projects.info>
2017-04-19 16:08:46 +0500

    [FIX] Fixes lint error

--- pos_order_cancel/static/src/js/cancel_order.js
+++ pos_order_cancel/static/src/js/cancel_order.js
@@ -115,7 +115,7 @@ odoo.define('pos_order_cancel', function (require) {
     var ConfirmCancellationPopupWidget = PopupWidget.extend({
         template: 'ConfirmCancellationPopupWidget',
         show: function(options){
-            options = options || {};
+            var options = options || {};
             this._super(options);
             if (options.reasons) {
                 this.events["click .cancelled-reason .button"] = "click_cancelled_reason";
