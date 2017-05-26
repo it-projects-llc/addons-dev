93dfb53d233264bc699543436a2c0b9643283edc
GabbasovDinar <gabbasov@it-projects.info>
2017-05-26 17:33:54 +0500

    [ADD] Added network printer status

--- pos_printer_network/models/pos_printers.py
+++ pos_printer_network/models/pos_printers.py
@@ -14,4 +14,9 @@ class PosConfig(models.Model):
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
+<<<<<<< HEAD
+    receipt_network_printer_ip = fields.Char(default=False, string="Receipt Network Printer IP", help="The ip address of the network printer for receipt, unused if left empty")
+    network_printer = fields.Boolean(default=False, string="Network Printer")
+=======
     receipt_network_printer_ip = fields.Char(default=False, string="Receipt Printer IP", help="The ip address of the network printer for receipt, unused if left empty")
+>>>>>>> c31c1f155ad527a7026fce173c14cfdb7c053f7d
