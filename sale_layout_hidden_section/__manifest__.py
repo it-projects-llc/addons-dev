e66e1e2142aa8812149e82cdc5f391aa9599cea6
ArtyomLosev <losev@it-projects.info>
2017-06-23 17:15:59 +0500

    [NEW] sale_layout_hidden_section module

---
+++ sale_layout_hidden_section/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Layout Hidden Section',
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Artyom Losev',
+    'license': 'LGPL-3',
+    'category': 'Custom',
+    'website': 'https://www.it-projects.info',
+    'description': """
+    	None
+    """,
+    'depends': ['sale'],
+    'data': [
+        'views.xml'
+    ],
+    'installable': True
+}
