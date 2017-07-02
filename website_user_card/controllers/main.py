d6660931313e582030998c6395cc77ac053e761c
Your Name <ololo0011@yandex.ru>
2017-07-02 18:14:08 +0500

    [REF] old code removed

--- website_user_card/controllers/main.py
+++ website_user_card/controllers/main.py
@@ -20,15 +20,10 @@ class website_user_card(http.Controller):
     def user(self, user_login):
 
         users = http.request.env['res.users'].sudo()
-        # partners = http.request.env['res.users.partner_id'].sudo()
-
-        # user1 = users.search([('login', '=', user_login)])
-        # user_id = partners.search([('partner_id', '=', user1.partner_id)])
         if len(user_login) == 0:
             location = '/page/users/'
             return werkzeug.utils.redirect(location)
         else:
             return http.request.render('website_user_card.test_user', {
                 'user': users.search([('login', '=', user_login)]),
-                # 'partner': partners.search([('login', '=', user_login)])
             })
