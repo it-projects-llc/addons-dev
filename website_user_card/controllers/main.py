196f586703bc06b986c0a3db01b366223cf8d593
Dinar <gabbasov@it-projects.info>
2017-07-06 11:37:32 +0500

    [ADD]  website_user_card (#231)
    * [ADD] Added test module by Alexander
    
    * [FIX] Removed *.pyc files
    
    * [FIX] display user data for unsigned
    
    * [REF] old code removed

---
+++ website_user_card/controllers/main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+import werkzeug
+
+class website_user_card(http.Controller):
+
+    @http.route('/page/user/', auth='public', website=True)
+    def redirect_index(self, **kw):
+        location = '/page/users/'
+        return werkzeug.utils.redirect(location)
+
+    @http.route('/page/users/', auth='public', website=True)
+    def index(self, **kw):
+        users = http.request.env['res.users'].sudo()
+        return http.request.render('website_user_card.uindex', {
+            'users': users.search([])
+        })
+
+    @http.route('/page/user/<string:user_login>', auth='public', website=True)
+    def user(self, user_login):
+
+        users = http.request.env['res.users'].sudo()
+        if len(user_login) == 0:
+            location = '/page/users/'
+            return werkzeug.utils.redirect(location)
+        else:
+            return http.request.render('website_user_card.test_user', {
+                'user': users.search([('login', '=', user_login)]),
+            })
