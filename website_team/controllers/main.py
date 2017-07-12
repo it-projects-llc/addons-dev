90e5fd2bf16bd61a93b24bdd56a815a498d409af
Your Name <kolushov@it-projects.info>
2017-07-12 15:28:33 +0500

    [IMP] alias name link

--- website_team/controllers/main.py
+++ website_team/controllers/main.py
@@ -12,19 +12,25 @@ class WebsiteTeam(http.Controller):
             'users': users.search([])
         })
 
-    @http.route('/team/<string:user_gh>', auth='public', website=True)
-    def user(self, user_gh):
+    @http.route('/team/<string:login>', auth='public', website=True)
+    def user(self, login):
 
-        current_user = http.request.env['res.users'].sudo().search([('username_github', '=', user_gh)])
+        current_user = http.request.env['res.users'].sudo().search([('username_github', '=', login)])
+
+        if current_user.id is False:
+            current_user = http.request.env['res.users'].sudo().search([('alias_name', '=', login)])
+            if current_user.username_github is not False and current_user.username_github != current_user.alias_name:
+                return werkzeug.utils.redirect("/team/" + current_user.username_github)
+
+        if current_user.id is False:
+            from werkzeug.exceptions import NotFound
+            raise NotFound()
+
         youtube_id = current_user.get_youtube_id(current_user.presentation_youtube_link)
         alias_email_full = current_user.alias_name + "@" + current_user.alias_domain
 
-        if len(user_gh) == 0:
-            location = '/team/'
-            return werkzeug.utils.redirect(location)
-        else:
-            return http.request.render('website_team.test_user', {
-                'user': current_user,
-                'youtube_link': youtube_id,
-                'alias_email': alias_email_full
-            })
+        return http.request.render('website_team.test_user', {
+                        'user': current_user,
+                        'youtube_link': youtube_id,
+                        'alias_email': alias_email_full
+                    })
