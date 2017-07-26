48ccd85634db0c05c89d05dd36645ca54fba63e1
GabbasovDinar <gabbasov@it-projects.info>
2017-07-26 20:08:45 +0500

    [FIX] lint error

--- pos_printer_network/static/src/js/network_printer.js
+++ pos_printer_network/static/src/js/network_printer.js
@@ -228,10 +228,11 @@ odoo.define('pos_restaurant.network_printer', function (require) {
                     }
                 }
                 msg = msg ? msg + ' ' + _t('Offline') : msg;
-                this.set_status(warning ?
-                    'warning' :
-                    'connected',
-                    msg);
+                this.set_status(
+                warning ?
+                'warning' :
+                'connected',
+                msg);
             } else {
                 this._super(status);
             }
