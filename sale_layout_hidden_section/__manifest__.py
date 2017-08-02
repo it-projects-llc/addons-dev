4d465a95986a2d222e2851f920ab48cad214c877
root <liliya@it-projects.info>
2017-08-02 13:42:37 +0000

    [DOC] edit index.html + add main image

--- sale_layout_hidden_section/__manifest__.py
+++ sale_layout_hidden_section/__manifest__.py
@@ -4,13 +4,16 @@
     "summary": """
         Allows to encapsulate layouts in sale orders.""",
     "category": "Sale",
-    "images": [],
+    "images": ['images/sale_layout_hidden_section.png'],
     "version": "1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Artyom Losev",
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info",
+    "price": 29.00,
+    "currency": 'EUR',
+
     "license": "LGPL-3",
 
     "depends": [
