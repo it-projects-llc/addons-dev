ca14c9463e456ffdfdfa0f7f659692e7612a143d
Ivan Yelizariev <yelizariev@it-projects.info>
2017-10-01 12:35:51 +0000

    [ADD] don't load mobile stuff if m=1 is not presented in url

--- pos_mobile/static/src/js/mobile.js
+++ pos_mobile/static/src/js/mobile.js
@@ -2,6 +2,7 @@ odoo.define('pos_mobile.mobile', function (require) {
     "use strict";
 
     var models = require('point_of_sale.models');
+    var session = require('pos_mobile.session');
 
 
 
@@ -9,21 +10,7 @@ odoo.define('pos_mobile.mobile', function (require) {
     var PosModelSuper = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
         initialize: function (session, attributes) {
-            var getUrlParameter = function getUrlParameter(sParam) {
-                var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-                    sURLVariables = sPageURL.split('&'),
-                    sParameterName,
-                    i;
-
-                for (i = 0; i < sURLVariables.length; i++) {
-                    sParameterName = sURLVariables[i].split('=');
-
-                    if (sParameterName[0] === sParam) {
-                        return sParameterName[1] === undefined ? true : sParameterName[1];
-                    }
-                }
-            };
-            this.is_mobile = getUrlParameter('m');
+            this.is_mobile = odoo.is_mobile;
             return PosModelSuper.initialize.call(this, session, attributes);
         },
     })
