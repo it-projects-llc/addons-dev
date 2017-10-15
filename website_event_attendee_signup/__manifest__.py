50d2842d74946c5a23ed192d9daaf095825cd38d
Ivan Yelizariev <yelizariev@it-projects.info>
2017-10-15 06:46:34 +0000

    [FIX] view file was not used

--- website_event_attendee_signup/__manifest__.py
+++ website_event_attendee_signup/__manifest__.py
@@ -22,6 +22,7 @@
     "external_dependencies": {"python": [], "bin": []},
     "data": [
         "views/event_event_views.xml",
+        "views/event_registration_views.xml",
         "data/mail_template_data.xml",
     ],
     "qweb": [
