b9ead4954e28010b73bc2ad8d190d5918377909c
Ivan Yelizariev <yelizariev@it-projects.info>
2017-10-15 12:43:48 +0000

    Works!

---
+++ website_event_attendee_fields/controllers/main.py
@@ -0,0 +1,8 @@
+from odoo import http, _
+from odoo.addons.website_event.controllers.main import WebsiteEventController
+from odoo.http import request
+
+
+class WebsiteEventSaleController(WebsiteEventController):
+
+    pass
