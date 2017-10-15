33e93ecca86246d65f9326cd21b7a7b9a00fd68c
Ivan Yelizariev <yelizariev@it-projects.info>
2017-10-15 13:00:32 +0000

    [LINT]
    remove unused controllers folder

--- website_event_attendee_fields/controllers/main.py
+++
@@ -1,8 +0,0 @@
-from odoo import http, _
-from odoo.addons.website_event.controllers.main import WebsiteEventController
-from odoo.http import request
-
-
-class WebsiteEventSaleController(WebsiteEventController):
-
-    pass
