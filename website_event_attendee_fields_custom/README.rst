cb82db3b9f747cb5a52d78bf1c015e6edadfc30b
Ivan Yelizariev <yelizariev@it-projects.info>
2017-10-15 13:33:44 +0000

    [NEW] website_event_attendee_fields_custom

---
+++ website_event_attendee_fields_custom/README.rst
@@ -0,0 +1,35 @@
+===============
+ Attendee registration: Birthdate, Passport, Nationality
+===============
+
+Ask information on registration and stores at Partner record
+
+{Then add more detailed description, technical specifications, any other information that could be interested for other developers. Don't forget that Usage instructions is a separated and has to be located in doc/index.rst file.}
+
+Credits
+=======
+
+Contributors
+------------
+* `Ivan Yelizariev <https://it-projects.info/team/yelizariev>`__
+
+Sponsors
+--------
+* `IT-Projects LLC <https://it-projects.info>`__
+
+Maintainers
+-----------
+* `IT-Projects LLC <https://it-projects.info>`__
+
+Further information
+===================
+
+Demo: http://runbot.it-projects.info/demo/website-addons/10.0
+
+HTML Description: https://apps.odoo.com/apps/modules/10.0/website_event_attendee_fields_custom/
+
+Usage instructions: `<doc/index.rst>`_
+
+Changelog: `<doc/changelog.rst>`_
+
+Tested on Odoo 10.0 51861e889ab7c8795cccc3eaca27b90b62ceb89c
---
+++ website_event_attendee_fields_custom/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Attendee registration: Birthdate, Passport, Nationality""",
+    "summary": """Ask information on registration and stores at Partner record""",
+    "category": "Marketing",
+    # "live_test_url": "",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "{DEPENDENCY1}",
+        "{DEPENDENCY2}",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "{FILE1}.xml",
+        "{FILE2}.xml",
+    ],
+    "qweb": [
+        "static/src/xml/{QWEBFILE1}.xml",
+    ],
+    "demo": [
+        "demo/{DEMOFILE1}.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
