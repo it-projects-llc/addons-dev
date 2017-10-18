c22cebb084e0ad2740b041477d0f4fc857c5c53a
GabbasovDinar <gabbasov@it-projects.info>
2017-10-18 18:57:02 +0500

    [IMP] add new styles, load css only mobile version

--- pos_mobile/static/src/js/screens.js
+++ pos_mobile/static/src/js/screens.js
@@ -5,10 +5,51 @@ odoo.define('pos_mobile.screens', function (require) {
 
     var screens = require('point_of_sale.screens');
 
+    screens.ProductCategoriesWidget.include({
+        init: function(parent, options){
+            this._super(parent,options);
+            var self = this;
+            this.show_category_list = true;
+            this.show_order_action_buttons = true;
+            this.change_category_display = function(event){
+                if (self.show_category_list) {
+                    $( '.pos.mobile .categories' ).slideDown();
+                    self.show_category_list = false;
+                } else {
+                    $( '.pos.mobile .categories' ).slideUp();
+                    self.show_category_list = true;
+                }
+            };
+            this.change_action_order_buttons_display = function(event){
+                if (self.show_order_action_buttons) {
+                    $( '.pos.mobile .pads' ).slideDown();
+                    self.show_order_action_buttons = false;
+                    $('.fa-plus-square-o').css('display','none');
+                    $('.fa-minus-square-o').css('display','inline-block');
+                } else {
+                    $( '.pos.mobile .pads' ).slideUp();
+                    $('.fa-minus-square-o').css('display','none');
+                    $('.fa-plus-square-o').css('display','inline-block');
+                    self.show_order_action_buttons = true;
+                }
+            };
+        },
+        renderElement: function(){
+            this._super.apply(this, arguments);
+            var category_display_button = this.el;
+            var category_button = this.el.querySelector('.category-list-button');
+            category_button.addEventListener('click', this.change_category_display);
+            var action_button = this.el.querySelector('.order-action-buttons');
+            action_button.addEventListener('click', this.change_action_order_buttons_display);
+        },
+    });
+
     screens.ProductScreenWidget.include({
         renderElement: function () {
             this._super.apply(this, arguments);
+            var self = this;
             this.pos.ready.then(function(){
+                $('.pos').addClass('mobile');
                 var swiper = new Swiper('.swiper-container');
                 var elements = $(".swiper-slide");
                 var width = elements.css('width');
@@ -24,6 +65,23 @@ odoo.define('pos_mobile.screens', function (require) {
                 tr.detach();
                 rightpane.append(tr);
             });
+        },
+        click_product: function(product) {
+            this._super.apply(this, arguments);
+            var $p = $('span[data-product-id="'+product.id+'"]');
+            $($p).animate({
+                'opacity': 0.5,
+            }, 200, function(){ $($p).animate({
+                'opacity': 1,
+            }, 400) } );
+            var $pi = $('span[data-product-id="'+product.id+'"] img');
+            $($pi).animate({
+                'max-height': '120px',
+                'min-width': '100px',
+            }, 200, function(){ $($pi).animate({
+                'max-height': '100px',
+                'min-width': '60px',
+            }, 400) } );
         },
     });
     return screens;
