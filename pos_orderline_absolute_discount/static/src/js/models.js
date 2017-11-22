97be36a4884f629abf4cfbf42dbb261efffc6896
GabbasovDinar <gabbasov@it-projects.info>
2017-11-22 18:35:58 +0500

    [LINT] fix lint errors

--- pos_orderline_absolute_discount/static/src/js/models.js
+++ pos_orderline_absolute_discount/static/src/js/models.js
@@ -31,7 +31,7 @@ odoo.define('pos_absolute_discount.models', function(require){
                 this.set_discount(0);
             }
             this.absolute_discount = discount || 0;
-            this.absolute_discountStr = '' + this.absolute_discount;
+            this.absolute_discountStr = String(this.absolute_discount);
             this.trigger('change',this);
         },
         // returns the absolute discount
@@ -61,9 +61,8 @@ odoo.define('pos_absolute_discount.models', function(require){
             // we don't merge discounted orderlines
             if (this.get_absolute_discount() > 0) {
                 return false;
-            } else {
-                return _super_orderline.can_be_merged_with.apply(this, arguments);
             }
+            return _super_orderline.can_be_merged_with.apply(this, arguments);
         },
         export_as_JSON:function(){
             var res = _super_orderline.export_as_JSON.apply(this, arguments);
@@ -81,20 +80,19 @@ odoo.define('pos_absolute_discount.models', function(require){
         get_base_price: function(){
             var rounding = this.pos.currency.rounding;
             if (this.get_absolute_discount()) {
-                return round_pr((this.get_unit_price() * this.get_quantity() - this.get_absolute_discount()), rounding);
-            } else {
-                return _super_orderline.get_base_price.apply(this, arguments);
+                return round_pr(((this.get_unit_price() * this.get_quantity()) - this.get_absolute_discount()), rounding);
             }
+            return _super_orderline.get_base_price.apply(this, arguments);
         },
         get_all_prices: function(){
             var res = _super_orderline.get_all_prices.apply(this, arguments);
             if (this.get_absolute_discount()) {
-                var price_unit = this.get_unit_price() - this.get_absolute_discount() / this.get_quantity();
+                var price_unit = this.get_unit_price() - (this.get_absolute_discount() / this.get_quantity());
                 var taxtotal = 0;
 
-                var product =  this.get_product();
+                var product = this.get_product();
                 var taxes_ids = product.taxes_id;
-                var taxes =  this.pos.taxes;
+                var taxes = this.pos.taxes;
                 var taxdetail = {};
                 var product_taxes = [];
 
@@ -126,7 +124,8 @@ odoo.define('pos_absolute_discount.models', function(require){
         },
         add_product: function(product, options){
             _super_order.add_product.apply(this, arguments);
-            if(options && options.absolute_discount !== undefined){
+            var line = this.get_selected_orderline();
+            if(line && options && typeof options.absolute_discount !== "undefined"){
                 line.set_absolute_discount(options.absolute_discount);
             }
         },
