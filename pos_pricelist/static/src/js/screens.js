5663d5f66906fd0da0bfdb05b5c2a2e3d3b6d197
GabbasovDinar <gabbasov@it-projects.info>
2017-11-22 14:42:43 +0500

    [ADD] pos_pricelist and pos_orderline_absolute_discount

---
+++ pos_pricelist/static/src/js/screens.js
@@ -0,0 +1,73 @@
+/******************************************************************************
+*    Point Of Sale - Pricelist for POS Odoo
+*    Copyright (C) 2014 Taktik (http://www.taktik.be)
+*    @author Adil Houmadi <ah@taktik.be>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as
+*    published by the Free Software Foundation, either version 3 of the
+*    License, or (at your option) any later version.
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
+odoo.define('pos_pricelist.screens', function (require) {
+	"use strict";
+
+	var screens = require('point_of_sale.screens');
+	var core = require('web.core');
+
+    screens.ClientListScreenWidget.include({
+        save_changes: function () {
+            this._super();
+            if (this.has_client_changed()) {
+                var currentOrder = this.pos.get('selectedOrder');
+                var orderLines = currentOrder.orderlines.models;
+                var partner = currentOrder.get_client();
+                this.pos.pricelist_engine.update_products_ui(partner);
+                this.pos.pricelist_engine.update_ticket(partner, orderLines);
+                if (partner && partner.property_product_pricelist[0] !== this.pos.config.pricelist_id[0]) {
+                    var buttons = this.getParent().screens.products.action_buttons;
+                    if (buttons && buttons.pricelist) {
+                        if (orderLines.length) {
+                            orderLines.forEach(function(line){
+                                buttons.pricelist.set_change_pricelist_button(true, line)
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    });
+    var PricelistButton = screens.ActionButtonWidget.extend({
+        template: 'PricelistButton',
+        button_click: function() {
+            var order = this.pos.get_order();
+            var orderline = order.get_selected_orderline();
+            if (orderline && orderline.default_pricelist_is_active) {
+                this.apply_pricelist();
+            }
+        },
+        apply_pricelist: function() {
+            var order = this.pos.get_order();
+            var line = order.get_selected_orderline();
+            this.set_change_pricelist_button(false, line);
+            line.set_default_pricelist();
+        },
+        set_change_pricelist_button(status, line) {
+            this.highlight(status);
+            if (line) {
+                line.default_pricelist_is_active = status;
+            }
+        },
+    });
+
+    screens.define_action_button({
+        'name': 'pricelist',
+        'widget': PricelistButton,
+    });
+});
---
+++ pos_pricelist/static/src/js/widgets.js
@@ -0,0 +1,121 @@
+/******************************************************************************
+ *    Point Of Sale - Pricelist for POS Odoo
+ *    Copyright (C) 2014 Taktik (http://www.taktik.be)
+ *    @author Adil Houmadi <ah@taktik.be>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+odoo.define('pos_pricelist.widgets', function (require) {
+	"use strict";
+
+	var screens = require('point_of_sale.screens');
+	var PosBaseWidget = require('point_of_sale.BaseWidget');
+	var models = require('pos_pricelist.models');
+	var core = require('web.core');
+	var utils = require('web.utils');
+	var formats = require('web.formats');
+
+    var round_di = utils.round_decimals;
+
+    screens.OrderWidget.include({
+        init: function(parent, options) {
+            this._super(parent, options);
+            this.pos.bind('change:selectedLine', this.change_selected_line, this);
+        },
+        change_selected_line: function() {
+            var buttons = this.getParent().action_buttons;
+            var line = this.pos.get_order().get_selected_orderline();
+            if (buttons && buttons.pricelist) {
+                buttons.pricelist.set_change_pricelist_button(line.default_pricelist_is_active, line)
+            }
+        },
+        change_selected_order: function() {
+            this._super();
+            var order = this.pos.get_order();
+            var buttons = this.getParent().action_buttons;
+            if (order && buttons && buttons.pricelist) {
+                var line = order.get_selected_orderline();
+                if (line) {
+                    buttons.pricelist.set_change_pricelist_button(line.default_pricelist_is_active, line)
+                } else {
+                    buttons.pricelist.set_change_pricelist_button(false, false);
+                }
+            }
+        },
+        set_value: function (val) {
+            this._super(val);
+            var order = this.pos.get('selectedOrder');
+            if (!order.finalized && order.get_selected_orderline()) {
+                var mode = this.numpad_state.get('mode');
+                if (mode === 'price') {
+                    order.get_selected_orderline().set_manual_price(true);
+                    order.get_selected_orderline().set_old_unit_price(val);
+                }
+            }
+        }
+    });
+
+    screens.ActionButtonWidget = screens.ActionButtonWidget.extend({
+        selectOrder: function (event) {
+            this._super(event);
+            var partner = this.order.get_client() ?
+                          this.order.get_client() :
+                          false;
+            this.pos.pricelist_engine.update_products_ui(partner);
+        }
+    });
+
+    screens.ProductListWidget.include({
+        init: function (parent, options) {
+            this._super(parent, options);
+            this.iface_tax_included = false;
+            if (
+                posmodel
+                && posmodel.config
+                && posmodel.config.iface_tax_included
+            ) {
+                this.iface_tax_included
+                    = posmodel.config.iface_tax_included
+            }
+        },
+        renderElement: function () {
+            this._super();
+            var order = posmodel.get_order();
+            var customer = null;
+            if(order) {
+                customer = order.get_client();
+            }
+            this.pos.pricelist_engine.update_products_ui(customer);
+        }
+    });
+
+    PosBaseWidget.include({
+        format_pr: function(amount, precision) {
+            // Do not call _super because no addon or XML is using this method
+            var currency = (this.pos && this.pos.currency) ? this.pos.currency : {symbol:'$', position: 'after', rounding: 0.01, decimals: 2};
+            var decimals = currency.decimals;
+
+            if (precision && (typeof this.pos.dp[precision]) !== undefined) {
+                decimals = this.pos.dp[precision];
+            }
+
+            if (typeof amount === 'number') {
+                amount = round_di(amount,decimals).toFixed(decimals);
+                amount = formats.format_value(round_di(amount, decimals), { type: 'float', digits: [69, decimals]});
+            }
+            return amount
+        }
+    });
+
+	return screens;
+});
