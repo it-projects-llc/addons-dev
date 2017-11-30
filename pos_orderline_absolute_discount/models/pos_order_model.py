ed4a6bc2db0a9696c750e92783df684a6ec12612
GabbasovDinar <gabbasov@it-projects.info>
2017-11-30 15:27:03 +0500

    [FIX] taxes in pos.order

--- pos_orderline_absolute_discount/models/pos_order_model.py
+++ pos_orderline_absolute_discount/models/pos_order_model.py
@@ -6,6 +6,22 @@ from odoo.exceptions import UserError
 _logger = logging.getLogger(__name__)
 
 
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    @api.model
+    def _amount_line_tax(self, line, fiscal_position_id):
+        if line.absolute_discount:
+            taxes = line.tax_ids.filtered(lambda t: t.company_id.id == line.order_id.company_id.id)
+            if fiscal_position_id:
+                taxes = fiscal_position_id.map_tax(taxes, line.product_id, line.order_id.partner_id)
+            price = line.price_unit - line.absolute_discount
+            taxes = taxes.compute_all(price, line.order_id.pricelist_id.currency_id, line.qty, product=line.product_id, partner=line.order_id.partner_id or False)['taxes']
+            return sum(tax.get('amount', 0.0) for tax in taxes)
+        else:
+            return super(PosOrder, self)._amount_line_tax(line, fiscal_position_id)
+
+
 class PosOrderLine(models.Model):
     _inherit = "pos.order.line"
 
