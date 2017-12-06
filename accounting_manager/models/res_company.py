e2aac511af833cc830ffc259592843a58cf2c83f
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-06 15:33:56 +0000

    [ADD] accounting_manager: on creating new company all fact managers automatically get it in their allowed companies

--- accounting_manager/models/res_company.py
+++ accounting_manager/models/res_company.py
@@ -15,3 +15,11 @@ class Company(models.Model):
         # without it country field has not got its default value from ir.values default
         if self.state_id:
             self.country_id = self.state_id.country_id
+
+    @api.model
+    def create(self, vals):
+        company = super(Company, self).create(vals)
+        fact_managers = self.env['res.users'].search([('groups_id', 'in', [self.env.ref('accounting_manager.group_fact_manager').id])])
+        if fact_managers:
+            fact_managers.write({'company_ids': [(4, company.id, 0)]})
+        return company
