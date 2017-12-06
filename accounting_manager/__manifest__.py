d276b4830d808b4bdefc535a26b3d99d7b143f6d
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-06 14:36:47 +0000

    [ADD] accounting_manager: update allowed companies using base.action.rule

--- accounting_manager/__manifest__.py
+++ accounting_manager/__manifest__.py
@@ -18,6 +18,7 @@
     "depends": [
         "sale",
         "access_settings_menu",
+        "base_action_rule",
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
@@ -25,6 +26,7 @@
         'security/ir.model.access.csv',
         'views/res_config_view.xml',
         'data/ir_values_data.xml',
+        'data/base_action_rule_data.xml',
     ],
     "qweb": [
     ],
