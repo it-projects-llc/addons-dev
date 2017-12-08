07671e812a577e87097754bc158d4d1c7423f3af
GabbasovDinar <gabbasov@it-projects.info>
2017-12-08 10:57:34 +0500

    [LINT] fix lint error

--- pos_orderline_absolute_discount/static/src/js/models.js
+++ pos_orderline_absolute_discount/static/src/js/models.js
@@ -31,8 +31,8 @@ odoo.define('pos_absolute_discount.models', function(require){
             var self = this;
             if (this.price < discount) {
                 this.pos.gui.screen_instances.products.numpad.state.set({
-	                buffer: this.get_absolute_discount_str()
-	            });
+                    buffer: this.get_absolute_discount_str()
+                });
                 return this.pos.gui.show_popup('error',{
                     'title': _t("Warning"),
                     'body': _t("It is not allowed to create a credit by discount: " + discount + self.pos.currency.symbol),
