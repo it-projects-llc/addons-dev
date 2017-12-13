8940f33b1c0696c7e80bf675d21f358400fbc147
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-13 09:47:57 +0000

    [ADD] accounting_manager: temp solution to hide not invoicing settings - need to do it in python in a new module

--- accounting_manager/models/res_config.py
+++ accounting_manager/models/res_config.py
@@ -7,8 +7,14 @@ class ResConfigSettings(models.TransientModel):
 
     @api.multi
     def execute(self):
-        if self.env['res.users'].has_group('accounting_manager.group_fact_manager') and \
-           self._name == 'account.config.settings':
+        if self.env['res.users'].has_group('accounting_manager.group_fact_manager'):
             self = self.sudo()
 
         return super(ResConfigSettings, self).execute()
+
+    @api.model
+    def get_values(self):
+        if self.env['res.users'].has_group('accounting_manager.group_fact_manager'):
+            self = self.sudo()
+        res = super(ResConfigSettings, self).get_values()
+        return res
