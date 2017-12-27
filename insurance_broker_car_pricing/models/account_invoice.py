c2adedab992c3db5f3d1cb79e3d042c32b3dc99b
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-27 14:53:54 +0000

    [ADD] insurance_broker_car_pricing: If date_from and date_to are are empty: set “tomorrow” in start_date and set “today of year+1” in field end_date;

--- insurance_broker_car_pricing/models/account_invoice.py
+++ insurance_broker_car_pricing/models/account_invoice.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as DF
@@ -38,3 +39,11 @@ class AccountInvoiceLine(models.Model):
                     _("End Date shouldn't be less than today for invoice line with "
                         "Description '%s'.")
                     % (invline.name))
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('start_date') and not vals.get('end_date'):
+            vals['start_date'] = date.today() + timedelta(1)
+            vals['end_date'] = date.today() + relativedelta(years=1)
+        invline = super(AccountInvoiceLine, self).create(vals)
+        return invline
