7887a1bcfea518bd36873169f3d9e8791b219fed
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-27 07:17:24 +0000

    [ADD] insurance_broker_car_pricing: _get_fiscal_power, _find_insurance_product

--- insurance_broker_car_pricing/models/fleet.py
+++ insurance_broker_car_pricing/models/fleet.py
@@ -10,11 +10,22 @@ class FleetVehicle(models.Model):
 
     def _get_fiscal_power(self):
         for record in self:
-            record.product_attribute_value_id = self.env.ref('insurance_broker_car_pricing.product_attribute_fp_value_fp1').id
+            fiscal_power = self.env['insurance_broker.fiscal_power'].search([('fuel_type', '=', record.fuel_type),
+                                                                            ('horsepower_from', '<=', record.horsepower),
+                                                                            ('horsepower_to', '>=', record.horsepower)])
+            if fiscal_power:
+                record.product_attribute_value_id = fiscal_power[0].product_attribute_value_id.id
 
     def _find_insurance_product(self):
         for record in self:
-            record.product_id = self.env.ref('insurance_broker_car_pricing.product_product_cat1_fp2').id
+            AttrVal = self.env['product.attribute.value']
+            seats_products = AttrVal.search([('attribute_id', '=', self.env.ref('insurance_broker_car_pricing.product_attribute_seats').id),
+                                             ('name', '=', str(record.seats))]).product_ids
+            fiscal_power_products = record.product_attribute_value_id.product_ids
+            products = seats_products & fiscal_power_products
+            products.filtered(lambda r: r.categ_id == record.category_id.id)
+            if products:
+                record.product_id = products[0].id
 
     category_id = fields.Many2one('product.category', 'Category', required=True)
     product_attribute_value_id = fields.Many2one('product.attribute.value', 'Fiscal Power',
