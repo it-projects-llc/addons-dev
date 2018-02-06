3bb543b793e5e75f810af63d5954c22f02754fcd
Ildar Nasyrov <nasyrov@it-projects.info>
2018-02-06 16:36:06 +0000

    [CI] ir_rule_website: refactor tests

--- ir_rule_website/tests/test_compute_domain.py
+++ ir_rule_website/tests/test_compute_domain.py
@@ -6,17 +6,18 @@ class TestComputeDomain(TransactionCase):
     at_install = True
     post_install = True
 
+    def setUp(self):
+        super(TestComputeDomain, self).setUp()
+        self.demo_user = self.env.ref( 'base.user_demo')
+        self.env['ir.rule'].create({'name': 'test ir_rule_website',
+                                    'model_id': self.env.ref('base.model_res_partner').id,
+                                    'domain_force': "[('parent_id', 'in', [website_id])]"})
+
+    def _cache_compute_domain(self, website_id):
+        test_domain = ('parent_id', 'in', [website_id])
+        domain = self.env['ir.rule'].sudo(user=self.demo_user.id).with_context(website_id=website_id)._compute_domain('res.partner')
+        self.assertTrue(test_domain in domain)
+
     def test_cache(self):
-        IrRule = self.env['ir.rule']
-        rule = IrRule.create({'name': 'test ir_rule_website',
-                              'model_id': self.env.ref('base.model_res_partner').id,
-                              'domain_force': "[('parent_id', 'in', [website_id])]"})
-        demo_user = self.env.ref('base.group_system')
-
-        def test_cache_compute_domain(website_id):
-            test_domain = ('parent_id', 'in', [website_id])
-            domain = IrRule.sudo(user=demo_user.id).with_context(website_id=website_id)._compute_domain('res.partner')
-            self.assertTrue(test_domain in domain)
-
-        test_cache_compute_domain(1)
-        test_cache_compute_domain(2)
+        self._cache_compute_domain(1)
+        self._cache_compute_domain(2)
