0b8dbce02e8cdd4632bc7beff627e74750a34103
Aleksandr <kolushov@it-projects.info>
2018-03-20 20:51:52 +0500

    [NEW] pos_discount_absolute module (it-projects-llc/addons-dev#370)
    * [DEV] absolute_discount [CI]
    
    * [IMP] condition based on the permanent attribute [FIX] possible bug on a floor screen
    
    * [ADD] recalculation option

--- pos_discount_absolute/models/pos_disc_abs_models.py
+++ pos_discount_absolute/models/pos_disc_abs_models.py
@@ -7,6 +7,8 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     discount_abs_value = fields.Float(string='Absolute Discount value', default=0, help='The default discount value')
-    discount_abs_enabled = fields.Boolean(string='Use absolute discount type', default=False, help='Discount type')
+    discount_abs_enabled = fields.Boolean(string='Absolute Discount', default=False, help='Enable Absolute discount in POS')
     discount_abs_product_id = fields.Many2one('product.product', string='Absolute Discount Product', domain="[('available_in_pos', '=', True)]", help='The product used to model an absolute discount')
     discount_product_id = fields.Many2one(string='Relative Discount Product', help='The product used to model a relative discount')
+    discount_abs_recalculation = fields.Boolean(string='Recalculation', help='Recalculation of the absolut discount after any changes in order.'
+                                                                             'Does not allow to set absolute discount higher than order total')
