6f7c5f2c01f32ac48e9cc47126f4c5bfccd89534
Aleksandr <kolushov@it-projects.info>
2018-03-20 20:51:52 +0500

    [NEW] pos_discount_absolute module (it-projects-llc/addons-dev#370)
    * [DEV] absolute_discount [CI]
    
    * [IMP] condition based on the permanent attribute [FIX] possible bug on a floor screen
    
    * [ADD] recalculation option

--- pos_discount_absolute/static/src/js/pos_discount_absolute.js
+++ pos_discount_absolute/static/src/js/pos_discount_absolute.js
@@ -16,8 +16,10 @@ odoo.define('pos_discount_absolute', function (require) {
         show: function (options) {
             var self = this;
             this._super(options);
+            // popup_abs_discount prevents errors on a page loading and exclude discount stuff rendering in other popups
             this.popup_abs_discount = false;
-            if (this.pos.config.discount_abs_enabled) {
+            this.pos.discount_abs_type = false;
+            if (options.abs_disc) {
                 self.popup_abs_discount = true;
                 self.events["click .absolute.button"] = "click_absolute_discount";
                 self.events["click .percentage.button"] = "click_percentage_discount";
@@ -25,10 +27,12 @@ odoo.define('pos_discount_absolute', function (require) {
         },
         click_absolute_discount: function() {
             this.pos.discount_abs_type = true;
+            this.inputbuffer = this.pos.config.discount_abs_value.toString();
             this.renderElement("Absolute Discount");
         },
         click_percentage_discount: function() {
             this.pos.discount_abs_type = false;
+            this.inputbuffer = this.pos.config.discount_pc.toString();
             this.renderElement("Discount Percentage");
         },
         renderElement: function(options){
@@ -57,17 +61,22 @@ odoo.define('pos_discount_absolute', function (require) {
                 this._super(val);
             }
         },
+        discount_button_click: function() {
+            var self = this;
+            if (this.pos.config.discount_abs_enabled) {
+                this.discount_options.abs_disc = true;
+            }
+            this._super();
+        },
         confirm_discount: function(val) {
             if (this.pos.discount_abs_type){
                 this.apply_absolute_discount(val);
+            } else if (this.abs_disc_presence()) {
+                var abs_disc = this.remove_abs_discount();
+                this._super(val);
+                this.apply_absolute_discount(this.pos.discount_abs_value || abs_disc);
             } else {
-                if (this.abs_disc_presence()) {
-                    this.remove_abs_discount();
-                    this._super(val);
-                    this.apply_absolute_discount(this.pos.discount_abs_value);
-                } else {
-                    this._super(val);
-                }
+                this._super(val);
             }
         },
         apply_absolute_discount: function(val){
@@ -75,9 +84,11 @@ odoo.define('pos_discount_absolute', function (require) {
             var order = this.pos.get_order();
             var abs_disc_product = this.pos.db.get_product_by_id(this.pos.config.discount_abs_product_id[0]);
             this.remove_abs_discount();
-            var discount = - Math.min(val, order.get_total_with_tax());
-            if (val !== 0){
-                order.add_product(abs_disc_product, { price: discount });
+            var discount = this.pos.config.discount_abs_recalculation
+                ? - Math.max(Math.min(val, order.get_total_with_tax()), 0)
+                : - Math.abs(val);
+            if (val && val !== 0){
+                order.add_product(abs_disc_product, { price: - Math.abs(discount) });
             }
         },
         abs_disc_presence: function() {
@@ -95,13 +106,19 @@ odoo.define('pos_discount_absolute', function (require) {
         remove_abs_discount: function(){
             var order = this.pos.get_order();
             var abs_disc_product = this.pos.db.get_product_by_id(this.pos.config.discount_abs_product_id[0]);
-            var abs_disc_product_price = abs_disc_product.price;
+            var abs_disc_product_price = _.find(order.get_orderlines(), function(pl) {
+                return pl.product.id === abs_disc_product.id;
+            });
+            if (!abs_disc_product_price) {
+                return false;
+            }
+            abs_disc_product_price = abs_disc_product_price.get_display_price();
             if (abs_disc_product.taxes_id.length){
                 this.pos.taxes_on_discounts = true;
             }
             var lines = order.get_orderlines();
-            lines.forEach( function(line){
-                if (line.get_product() === abs_disc_product){
+            _.each(lines, function(line){
+                if (line && line.get_product() === abs_disc_product){
 
                     order.remove_orderline(line);
                 }
@@ -111,21 +128,21 @@ odoo.define('pos_discount_absolute', function (require) {
         update_summary: function(){
             var order = this.pos.get('selectedOrder');
             this._super();
-            if (!this.pos.config.discount_abs_enabled || !order.get_orderlines().length){
+            if (!this.pos.config.discount_abs_enabled || !order.get_orderlines().length || !this.pos.config.discount_abs_recalculation){
                 return;
             }
             var total = order
                 ? order.get_total_with_tax()
                 : 0;
             var abs_prod_id = this.pos.config.discount_abs_product_id[0];
-            var abs_disc_prod = this.pos.get_order().get_orderlines().find(function(line){
+            var abs_disc_prod = _.find(this.pos.get_order().get_orderlines(), function(line){
                 return abs_prod_id === line.product.id;
             });
             // taxes_on_discounts is needed to prevent endless cycle caused by negative price due to negative taxes
             if (abs_disc_prod && !this.pos.taxes_on_discounts){
                 var recalc = - abs_disc_prod.price !== (Number(this.pos.discount_abs_value) || 0);
                 if (( total < 0 || (recalc && total > 0)) ){
-                    this.apply_absolute_discount(Math.max(Math.abs(abs_disc_prod.price),this.pos.discount_abs_value || ''))
+                    this.apply_absolute_discount(Math.max(Math.abs(abs_disc_prod.price),this.pos.discount_abs_value || ''));
                 }
                 total = order
                     ? order.get_total_with_tax()
@@ -152,7 +169,8 @@ odoo.define('pos_discount_absolute', function (require) {
             discount = new_disc.toString();
             OrderlineSuper.prototype.set_discount.apply(this, [discount]);
             var products_widgets = this.pos.gui.screen_instances.products;
-            if (this.pos.config.discount_abs_enabled && products_widgets &&
+            if (this.pos.config.discount_abs_enabled && this.pos.gui.current_screen &&
+                this.pos.gui.current_screen.numpad && products_widgets &&
                 products_widgets.action_buttons && products_widgets.action_buttons.discount){
                 this.order.select_orderline(this);
                 this.pos.gui.current_screen.numpad.state.set({mode: "discount"});
@@ -160,11 +178,13 @@ odoo.define('pos_discount_absolute', function (require) {
         },
         get_display_price: function(){
             if (this.pos.config.iface_tax_included) {
-                return this.get_price_with_tax() === 0
+                return this.get_price_with_tax() === 0 && this.quantity && this.discount > 0 &&
+                ( this.pos.gui.chrome.screens.receipt && this.pos.gui.chrome.screens.receipt.$el.hasClass('oe_hidden') )
                     ? _t("FREE")
                     : this.get_price_with_tax();
             }
-            return this.get_base_price() === 0
+            return this.get_base_price() === 0 && this.quantity && this.discount > 0 &&
+            ( this.pos.gui.chrome.screens.receipt && this.pos.gui.chrome.screens.receipt.$el.hasClass('oe_hidden') )
                 ? _t("FREE")
                 : this.get_base_price();
         },
--- pos_discount_absolute/tests/test_pos_discount_absolute.py
+++ pos_discount_absolute/tests/test_pos_discount_absolute.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import odoo.tests
+from odoo.api import Environment
 
 
-@odoo.tests.common.at_install(False)
+@odoo.tests.common.at_install(True)
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
 
     def test_pos_discounts(self):
-        self.phantom_js("/pos/web",
-                        "odoo.__DEBUG__.services['web_tour.tour'].run('pos_abs_discount_tour')",
+        # needed because tests are run before the module is marked as
+        # installed. In js web will only load qweb coming from modules
+        # that are returned by the backend in module_boot. Without
+        # this you end up with js, css but no qweb.
+        cr = self.registry.cursor()
+        env = Environment(cr, self.uid, {})
+        env['ir.module.module'].search([('name', '=', 'pos_discount_absolute')], limit=1).state = 'installed'
+        cr.release()
+        self.phantom_js("/web",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('pos_abs_discount_tour', 500)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.pos_abs_discount_tour.ready",
                         login="admin")
