0c697dd31abfd7c55f4ed80649a423962d6b9b29
ArtyomLosev <losev@it-projects.info>
2018-03-21 15:07:31 +0500

    [11.0][PORT] pos_invoice_pay to 11.0 (it-projects-llc/addons-dev#365)
    * [11.0][PORT] pos_invoice_pay to 11.0
    
    * [11.0][PORT] pos debt notebook port (#557)
    
    * [DOC] pos_order_note: new price and demo url (#564)
    
    * [11.0][PORT] pos_longpolling, pos_multi_session, pos_multi_session_restaurant (#555)
    
    * [11.0][PORT] porting pos_longpolling from 10.0 to 11.0
    
    * [11.0][PORT] porting pos_multi_session and pos_multi_session_sync
    
    * [11.0][PORT] porting pos_multi_session_restaurant from 10.0 to 11.0
    
    * [ADD] pos_invoice_pay 11.0 config buttons
    
    * [FIX] UI fix: preventing to open multiple lines
    
    * [ADD] pos_longpolling
    
    * [LINT] lint fixes

---
+++ pos_invoice_pay/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """POS Invoice Pay""",
+    "summary": """Paying invoiceable Sales Orders and confirmed Invoies over Point of Sale""",
+    "category": "pos",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Artyom Losev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+
+    "depends": [
+        "account",
+        "base_automation",
+        "bus",
+        "point_of_sale",
+        "sale",
+        "pos_longpolling"
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "data.xml",
+        "actions/base_action_rules.xml",
+        "report/report.xml",
+        "view.xml"
+    ],
+    "qweb": [
+        'static/src/xml/pos.xml'
+        ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
