8c193a9af9b0e0a730b1cec5dfaf649eba392acb
Aleksandr <kolushov@it-projects.info>
2018-04-13 14:51:29 +0500

    [11.0][PORT] sale_layout_hidden_section (it-projects-llc/addons-dev#399)
    * [11.0][PORT] sale_layout_hidden_section
    
    * [LINT] correct version format in manifest file
    
    * [DOC] license in readme

---
+++ sale_layout_hidden_section/__manifest__.py
@@ -0,0 +1,34 @@
+# Copyright 2017 Artyom Losev
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Hidden Layouts for Sale Orders""",
+    "summary": """
+        Use layouts within specific Sale Order only""",
+    "category": "Sale",
+    "images": ['images/sale_layout_hidden_section.png'],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Artyom Losev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "price": 29.00,
+    "currency": 'EUR',
+
+    "license": "LGPL-3",
+
+    "depends": [
+        "sale_management",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        'views/views.xml'
+    ],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
---
+++ sale_layout_hidden_section/doc/index.rst
@@ -0,0 +1,16 @@
+================================
+ Hidden Layouts for Sale Orders
+================================
+
+Installation
+============
+
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
+
+
+Configuration
+=============
+
+* Open menu ``Sales >> Settings``
+* Turn on **Sections on Sales Orders**
+* Click ``[Save]``
