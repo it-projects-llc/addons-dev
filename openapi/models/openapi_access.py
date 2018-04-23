9c005ecd06a6ab92e5158d14496753d127ea7f16
Ivan Yelizariev <yelizariev@it-projects.info>
2018-04-24 00:52:10 +0500

    [DEV] designing models for configuration

---
+++ openapi/models/openapi_access.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from odoo import models, fields, api
+
+
+class Access(models.Model):
+    _name = 'openapi.access'
+    _description = 'Access via API '
+
+    active = fields.Boolean('Active', default=True)
+    namespace_id = fields.Many2one('openapi.namespace', 'Integration', required=True)
+    model_id = fields.Many2one('ir.model', 'Model', required=True)
+    model = fields.Char(related='model_id.model')
+    api_create = fields.Boolean('Create via API', default=False)
+    api_read = fields.Boolean('Read via API', default=False)
+    api_update = fields.Boolean('Update via API', default=False)
+    api_delete = fields.Boolean('Delete via API', default=False)
+    read_one_id = fields.Many2one(
+        'ir.exports',
+        'Read One Fields',
+        help='Fields to return on reading one record',
+        domain="[('resource', '=', model)]")
+    read_many_id = fields.Many2one(
+        'ir.exports',
+        'Read Many Fields',
+        help='Fields to return on reading via non one-record endpoint',
+        domain="[('resource', '=', model)]")
+    create_context_ids = fields.Many2many(
+        'openapi.access.create.context',
+        'Creation Context Presets',
+        help="Can be used to pass default values or custom context",
+        domain="[('model_id', '=', model_id)]",
+        context="{'default_model_id': model_id}",
+    )
+
+    _sql_constraints = [
+        ('namespace_model_uniq',
+         'unique (namespace_id, model_id)',
+         'There is already a record for this Model')
+    ]
+
+    @api.multi
+    def name_get(self):
+        return [(record.id, "%s/%s" % (record.namespace_id.name, record.model))
+                for record in self]
+
+
+class AccessCreateContext(models.Model):
+    _name = 'openapi.access.create.context'
+    _description = 'Context on creating via API '
+
+    name = fields.Char('Name')
+    description = fields.Char('Description')
+    model_id = fields.Many2one('ir.model', 'Model', required=True)
+    context = fields.Text('Context')
---
+++ openapi/models/openapi_namespace.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from odoo import models, fields, api
+
+
+class Namespace(models.Model):
+
+    _name = 'openapi.namespace'
+
+    active = fields.Boolean('Active', default=True)
+    name = fields.Char(
+        'Name',
+        required=True,
+        help="""Integration name, e.g. ebay, amazon, magento, etc.
+        The name is used in api endpoint""")
+    description = fields.Char('Description')
+    log_ids = fields.One2many('openapi.log', 'namespace_id', string='Logs')
+    log_request = fields.Selection([
+        ('disabled', 'Disabled'),
+        ('info', 'Short'),
+        ('debug', 'Full'),
+    ], 'Log Requests', default='info')
+
+    log_response = fields.Selection([
+        ('disabled', 'Disabled'),
+        ('error', 'Errors only'),
+        ('debug', 'Full'),
+    ], 'Log Responses', default='error')
+
+    last_log_date = fields.Datetime(
+        related='log_ids.create_date',
+        string='Latest usage'
+    )
+
+    @api.multi
+    def name_get(self):
+        return [(record.id, "/api/v1/%s%s" % (record.name,
+                                              ' (%s)' % record.description
+                                              if record.description else ''))
+                for record in self]
