3e1b48d5c4fd29c6cf32ee48edaaada25a7adc8b
GabbasovDinar <gabbasov@it-projects.info>
2018-04-30 17:26:01 +0500

    [REF] refactoring code for pos_orders_history

--- pos_orders_history/models/pos_orders_history_models.py
+++ pos_orders_history/models/pos_orders_history_models.py
@@ -16,6 +16,7 @@ class PosConfig(models.Model):
     current_day_orders_only = fields.Boolean("Current Day Orders Only", help="Show current day orders only", default=True)
     show_cancelled_orders = fields.Boolean("Show Cancelled Orders", default=True)
     show_posted_orders = fields.Boolean("Show Posted Orders", default=False)
+    show_barcode_in_receipt = fields.Boolean("Show Barcode in Receipt", default=True)
 
     # ir.actions.server methods:
     @api.model
