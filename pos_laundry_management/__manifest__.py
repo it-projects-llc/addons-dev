5e0009cbc0cb7bd536709261b9400d377a5f37cc
KolushovAlexandr <kolushov@it-projects.info>
2018-05-10 18:40:25 +0500

    [NEW] pos_laundry_management

---
+++ pos_laundry_management/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Laundry/Dry Cleaning Managment""",
+    "summary": """{SHORT_DESCRIPTION_OF_THE_MODULE}""",
+    "category": "Point of Sale",
+    # "live_test_url": "",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Kolushov Alexandr",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/KolushovAlexandr",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "point_of_sale",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        # "{FILE1}.xml",
+        # "{FILE2}.xml",
+    ],
+    "demo": [
+        # "demo/{DEMOFILE1}.xml",
+    ],
+    "qweb": [
+        # "static/src/xml/{QWEBFILE1}.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
