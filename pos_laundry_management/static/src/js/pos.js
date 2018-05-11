3ee0854eb6db3b2e0dc0dfdb08b51f12bd678cea
KolushovAlexandr <kolushov@it-projects.info>
2018-05-11 16:06:01 +0500

    [DEV] on changing qty for lots

--- pos_laundry_management/static/src/js/pos.js
+++ pos_laundry_management/static/src/js/pos.js
@@ -15,4 +15,38 @@ odoo.define('pos_laundry_management.pos', function (require) {
     var QWeb = core.qweb;
     var _t = core._t;
 
+
+    screens.NumpadWidget.include({
+        clickAppendNewChar: function(event) {
+            if (this.pos.get_order().selected_orderline.has_product_lot) {
+                var pack_lot_lines_length =  this.pos.get_order().selected_orderline.compute_lot_lines().length;
+                var newChar = 0;
+                newChar = Math.max(event.currentTarget.innerText || event.currentTarget.textContent,
+                                   pack_lot_lines_length);
+                var res = this.state.appendNewChar(newChar);
+                this.pos.gui.show_popup('packlotline', {
+                    'title': _t('Lot/Serial Number(s) Required'),
+                    'pack_lot_lines': this.pos.get_order().selected_orderline.compute_lot_lines(),
+                    'order': this.pos.get_order()
+                });
+                return res;
+            }
+            return this._super.apply(this, arguments);
+        },
+
+        clickDeleteLastChar: function() {
+            var res = this._super.apply(this, arguments);
+            if (this.pos.get_order().selected_orderline.has_product_lot) {
+                this.pos.gui.show_popup('packlotline', {
+                    'title': _t('Lot/Serial Number(s) Required'),
+                    'pack_lot_lines': this.pos.get_order().selected_orderline.compute_lot_lines(),
+                    'order': this.pos.get_order()
+                });
+                return undefined;
+            }
+            return this._super.apply(this, arguments);
+        },
+
+    })
+
 });
