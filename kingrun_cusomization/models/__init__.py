6cf277cf33368609c813e88ec1065e69e7ecf1db
KolushovAlexandr <kolushov@it-projects.info>
2018-05-14 18:49:44 +0500

    [DEV] kingrun_cusomization

---
+++ kingrun_cusomization/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+import mrp
+import stock_warehouse
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
