a771540dcfe9190cbfcb9b705fd7e94c07cc959e
Aleksandr <kolushov@it-projects.info>
2018-05-22 00:10:59 +0500

    [10.0][ADD] delivery buttons in order history [IMP] non-scrollable notebook line (it-projects-llc/addons-dev#436)
    * [ADD] delivery buttons in order history
    
    * [IMP] static not scrollable notebook line
    
    * [IMP] delivery1 only for products in waiting delivery
    
    * [DEV] updating branch with pos order history return
    
    * [FIX] state done behavior in the order history screen

--- pos_laundry_management/models/models.py
+++ pos_laundry_management/models/models.py
@@ -30,6 +30,7 @@ class ResPartner(models.Model):
                     * complete_name
                  * state
                  * partner_id
+                 * order_id
         """
         fields = [
             'date',
@@ -58,6 +59,7 @@ class ResPartner(models.Model):
                 for wh in line['warehouse_ids']:
                     whs.append(self.env["stock.warehouse"].browse(wh).lot_stock_id)
                 line['warehouse_ids'] = map(lambda w: w.complete_name, whs)
+                line['order_id'] = self.env['pos.order'].search([('pos_reference', '=', line['receipt_barcode'])]).id
         return data
 
 
@@ -70,12 +72,21 @@ class MRPProduction(models.Model):
         readonly=True, states={'confirmed': [('readonly', False)]},
         help="Bill of Materials allow you to define the list of required raw materials to make a finished product.")
 
-    @api.one
+    @api.multi
     def set_state(self, new_state):
-        self.write({
-            'state': new_state,
-        })
-        return self.env['res.partner'].browse(self.partner_id.id).load_history(None)
+        for prod in self:
+            prod.write({
+                'state': new_state,
+            })
+
+    @api.multi
+    def set_state_reload_history(self, new_state):
+        res = {}
+        self.set_state(new_state)
+        for prod in self:
+            partner = prod.partner_id
+            res[partner.id] = partner.load_history(None)
+        return res
 
 
 class PosOrder(models.Model):
@@ -105,6 +116,13 @@ class PosOrder(models.Model):
                         if partner_id:
                             production._onchange_partner_id()
         return order_ids
+
+    @api.one
+    def change_product_delivery_states(self, product_id, new_state):
+        mrps = self.env['mrp.production'].search([('receipt_barcode', '=', self.pos_reference),
+                                                  ('product_id', '=', product_id)])
+        mrps.set_state(new_state)
+        return mrps.ids
 
 
 class PosOrderLineLot(models.Model):
