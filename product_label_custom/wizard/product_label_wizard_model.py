c75b6b8e382466f93636e85ad8556a387a20aa69
Dinar <gabbasov@it-projects.info>
2018-05-21 17:42:55 +0500

    [10.0][IMP] product labels is available for product variants now; update simple product label (show purchase date) (it-projects-llc/addons-dev#439)

--- product_label_custom/wizard/product_label_wizard_model.py
+++ product_label_custom/wizard/product_label_wizard_model.py
@@ -17,6 +17,7 @@ class ProductLabelSettings(models.TransientModel):
     quantity = fields.Integer("Quantity", default=1)
     label_id = fields.Many2one('product.label', 'Label', help="Select label for the product",
                                default=_default_product_label)
+    purchase_id = fields.Many2one('purchase.order')
 
     @api.multi
     def get_paper_format_groups(self):
@@ -43,11 +44,16 @@ class ProductLabelWizard(models.TransientModel):
                     (0, 0, {'product_id': product.id, 'quantity': 1, 'label_id': product.default_label_id})
                     for product in self.env['product.template'].browse(product_ids)
                 ]
-
-            if model == 'purchase.order':
+            elif model == 'product.product':
+                product_ids = self._context.get('active_ids')
+                res = [
+                    (0, 0, {'product_id': product.product_tmpl_id.id, 'quantity': 1, 'label_id': product.product_tmpl_id.default_label_id})
+                    for product in self.env['product.product'].browse(product_ids)
+                ]
+            elif model == 'purchase.order':
                 order_ids = self._context.get('active_ids')
                 res = [
-                    (0, 0, {'product_id': order_line.product_id.product_tmpl_id.id, 'quantity': order_line.product_qty, 'label_id': order_line.product_id.product_tmpl_id.default_label_id})
+                    (0, 0, {'product_id': order_line.product_id.product_tmpl_id.id, 'quantity': order_line.product_qty, 'label_id': order_line.product_id.product_tmpl_id.default_label_id, 'purchase_id': order_line.order_id})
                     for order_line in self.env['purchase.order.line'].search([('order_id', 'in', order_ids)])
                 ]
         return res
