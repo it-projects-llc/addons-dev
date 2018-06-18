0a87908cb9e43e1cb819aaac11fc9bd45054721c
ILMIR <karamov@it-projects.info>
2018-06-19 01:34:40 +0500

    [FIX] runbot error

--- website_sale_address_custom/models/models.py
+++ website_sale_address_custom/models/models.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import models, fields, api
-import wdb
+# import wdb
 
 class Partner(models.Model):
     _inherit = "res.partner"
@@ -14,4 +14,4 @@ class Partner(models.Model):
     @api.model
     def upload_file(self, data):
         values = {}
-        wdb.set_trace()
+  #      wdb.set_trace()
