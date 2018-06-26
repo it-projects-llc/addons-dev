2057e0422b5d75791d4c9538d09a375954c0b780
Dinar <gabbasov@it-projects.info>
2018-06-26 18:42:25 +0500

    [10.0] :ambulance: compatibility pos_receipt_custom and pos_print_method; :heart_eyes: show waters on receipt (it-projects-llc/addons-dev#474)
    * [FIX] check order custom receipt
    
    * [IMP] show waters on receipt

--- pos_receipt_custom/models/model.py
+++ pos_receipt_custom/models/model.py
@@ -1,8 +1,14 @@
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-# -*- coding: utf-8 -*-
-from odoo import fields, models
+import io
+from odoo import fields, models, api
+from odoo.tools import image
+import codecs
+from PIL import Image
+Image.preinit()
+Image._initialized = 2
 
 
 class PosCustomReceipt(models.Model):
@@ -10,7 +16,23 @@ class PosCustomReceipt(models.Model):
 
     name = fields.Char('Name')
     type = fields.Selection(string="Type", selection=[('receipt', 'Receipt'), ('ticket', 'Ticket')])
+    image = fields.Binary("Image", attachment=True, help="This field holds the image used in receipt header")
+    printable_image = fields.Binary("Printable Image",
+                                    compute='_compute_printable_image', store=True,
+                                    help="This field holds the appropriate image for the receipt header")
     qweb_template = fields.Text('Qweb')
+
+    @api.depends('image')
+    def _compute_printable_image(self):
+        for rec in self:
+            if not rec.image:
+                return
+            image_stream = io.BytesIO(codecs.decode(rec.image, 'base64'))
+            image_open = Image.open(image_stream)
+            image_size = (min(image_open.size[0], 225),
+                          min(image_open.size[1], 225))
+
+            rec.printable_image = image.image_resize_image(rec.image, image_size)
 
 
 class PosConfig(models.Model):
@@ -27,7 +49,7 @@ class PosConfig(models.Model):
                                               help="Check box if you want to display the discount "
                                                    "of the orderline on the ticket")
 
-    custom_ticket = fields.Boolean(string="Custom", defaut=False)
+    custom_ticket = fields.Boolean(string="Custom Ticket", defaut=False)
     custom_ticket_id = fields.Many2one("pos.custom_receipt", string="Custom Template",
                                        domain=lambda self: self._get_custom_ticket_id_domain())
 
