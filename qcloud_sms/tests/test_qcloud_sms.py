0dd12180c0fb29fdb9da5e9307f5c2e6818ca0a4
GabbasovDinar <gabbasov@it-projects.info>
2018-08-15 18:28:02 +0500

    :fire_engine: fix test_send_message

--- qcloud_sms/tests/test_qcloud_sms.py
+++ qcloud_sms/tests/test_qcloud_sms.py
@@ -3,11 +3,6 @@
 import logging
 import requests_mock
 
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-
 from odoo.tests.common import HttpCase
 from odoo import api
 
@@ -23,15 +18,23 @@ class TestQCloudSMS(HttpCase):
         super(TestQCloudSMS, self).setUp()
         self.phantom_env = api.Environment(self.registry.test_cr, self.uid, {})
         self.Message = self.phantom_env['qcloud.sms']
-
+        self.partner = self.phantom_env['res.partner'].create({
+            'name': 'Test User',
+            'email': 'test@test.test',
+            'mobile': '861234567890'
+        })
+        self.message_template = self.phantom_env['qcloud.sms.template'].create({
+            'name': 'Test Template',
+            'sms_type': '0'
+        })
         self.requests_mock = requests_mock.Mocker(real_http=True)
         self.requests_mock.start()
         self.addCleanup(self.requests_mock.stop)
 
-    def _patch_post_requests(self, url, data, response_json):
-        self.requests_mock.register_uri('POST', url, request_headers=data, json=response_json)
+    def _patch_post_requests(self, url, response_json):
+        self.requests_mock.register_uri('POST', url, json=response_json)
 
-    def _send_simple_message(self, data):
+    def _send_simple_message(self, message):
         response_json = {
             "result": 0,
             "errmsg": "OK",
@@ -40,16 +43,13 @@ class TestQCloudSMS(HttpCase):
             "sid": "xxxxxxx"
         }
         base_url = 'https://yun.tim.qq.com/v5/tlssmssvr/sendsms'
-        self._patch_post_requests(base_url, data, response_json)
-        return self.Message.send_message(data)
+        self._patch_post_requests(base_url, response_json)
+        return self.Message.send_message(message, self.partner.id, template_id=self.message_template.id)
 
     def test_send_message(self):
         # sms_type: 0 - normal SMS, 1 - marketing SMS
-        data = {
-            'sms_type': 0,
-            'country_code': 0,
-            'phone_number': 123456789,
-            'msg': "Your verification code: 1234"
-        }
-        response = self._send_simple_message(data)
+        message = "Your login verification code is 1234, which is valid for 2 minutes." \
+                  "If you are not using our service, ignore the message."
+
+        response = self._send_simple_message(message)
         self.assertEquals(response.result, 0, 'Could not send message')
