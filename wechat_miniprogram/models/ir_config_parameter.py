d9f64556be7eb2a7a804e2a377b558f241c851ed
GabbasovDinar <gabbasov@it-projects.info>
2018-08-27 14:49:56 +0500

    :bomb: decryption data from mini-program

--- wechat_miniprogram/models/ir_config_parameter.py
+++ wechat_miniprogram/models/ir_config_parameter.py
@@ -2,8 +2,12 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 import logging
 import werkzeug.urls
+from Crypto.Cipher import AES
+import base64
+import json
 
-from odoo import models, api
+from odoo import models, api, _
+from odoo.exceptions import UserError
 
 
 _logger = logging.getLogger(__name__)
@@ -50,3 +54,22 @@ class Param(models.Model):
         url = '%s?%s' % (base_url, werkzeug.urls.url_encode(param))
         _logger.debug('openid url: %s', url)
         return url
+
+    def decrypt_wechat_miniprogram_data(self, encryptedData, iv):
+        # base64 decode
+        sessionKey = base64.b64decode(self.env.user.wechat_session_key)
+        encryptedData = base64.b64decode(encryptedData)
+        iv = base64.b64decode(iv)
+
+        cipher = AES.new(sessionKey, AES.MODE_CBC, iv)
+        decrypted = json.loads(self._unpad(cipher.decrypt(encryptedData)))
+
+        _logger.debug('Decrypt result: %s', decrypted)
+
+        if decrypted['watermark']['appid'] != self.sudo().get_param('wechat.miniprogram_app_id', ''):
+            raise UserError(_('Invalid Buffer'))
+
+        return decrypted
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s)-1:])]
