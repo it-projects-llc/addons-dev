21233f84987682ffe947656d4bbdf8b6c0487398
KolushovAlexandr <kolushov@it-projects.info>
2018-09-14 14:54:45 +0500

    :ambulance: added onchange methods for disable_payments, error with line removing when decrease amount is prohibited

--- pos_disable_payment/models.py
+++ pos_disable_payment/models.py
@@ -1,4 +1,13 @@
-from odoo import fields, models
+# Copyright 2015-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2016  manavi <https://github.com/manavi>
+# Copyright 2017  invitu <https://github.com/invitu>
+# Copyright 2017-2018  Ilmir Karamov <https://it-projects.info/team/ilmir-k>
+# Copyright 2017-2018  David Arnold <https://github.com/blaggacao>
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models, api
 
 
 class ResUsers(models.Model):
@@ -13,3 +22,8 @@ class ResUsers(models.Model):
     allow_create_order_line = fields.Boolean('Allow create order line', default=True)
     allow_refund = fields.Boolean('Allow refunds', default=True)
     allow_manual_customer_selecting = fields.Boolean('Allow manual customer selecting', default=True)
+
+    @api.onchange('allow_decrease_amount')
+    def _onchange_allow_decrease_amount(self):
+        if self.allow_decrease_amount is False:
+            self.allow_delete_order_line = False
--- pos_disable_payment_restaurant/models/model.py
+++ pos_disable_payment_restaurant/models/model.py
@@ -1,6 +1,7 @@
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResUsers(models.Model):
@@ -8,3 +9,14 @@ class ResUsers(models.Model):
 
     allow_decrease_kitchen_only = fields.Boolean('Allow change Qty for kitchen orders', default=True)
     allow_remove_kitchen_order_line = fields.Boolean('Allow remove kitchen order line', default=True)
+
+    @api.onchange('allow_delete_order_line')
+    def _onchange_allow_delete_order_line(self):
+        if self.allow_delete_order_line is False:
+            self.allow_remove_kitchen_order_line = False
+
+    @api.onchange('allow_decrease_amount')
+    def _onchange_allow_decrease_amount(self):
+        super(ResUsers, self)._onchange_allow_decrease_amount()
+        if self.allow_decrease_amount is True:
+            self.allow_decrease_kitchen_only = True
