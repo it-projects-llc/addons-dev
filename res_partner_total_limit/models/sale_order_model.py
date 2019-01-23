05cc06a788efb3b896fdc0084aa9d896045aedd4
Dinar <gabbasov@it-projects.info>
2019-01-23 17:39:04 +0500

    :zap: res_city_total_limit: delivery lead time and email notifications; res_partner_total_limit: add total and limit on partner form; display info on website shop and product form (it-projects-llc/addons-dev#598)
    * :bomb: new features for res_city_total_limit
    
    * :bomb: new features for res_partner_total_limit
    
    * :rose: field name and description

--- res_partner_total_limit/models/sale_order_model.py
+++ res_partner_total_limit/models/sale_order_model.py
@@ -4,7 +4,37 @@
 from odoo import fields, models
 
 
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
 
     amount_taken_into_account_user = fields.Boolean(default=False)
+
+    def action_updated_sale_order_partner(self):
+        if self.state == 'sale' and self.marketplace_seller_id and self.marketplace_seller_id.seller:
+            lines = self.search([('marketplace_seller_id', '=', self.marketplace_seller_id.id),
+                                 ('state', '=', 'sale'), ('amount_taken_into_account_user', '=', False)])
+            confirmed_order_lines_total_price = 0
+            confirmed_order_lines_total_price += sum(line.price_total for line in lines)
+
+            if confirmed_order_lines_total_price >= self.marketplace_seller_id.limit:
+                # TODO: from_to
+                # TODO: send only one message
+                composer = self.env['mail.compose.message'].with_context({
+                    'default_composition_mode': 'comment',
+                    'default_model': self._name,
+                    'default_res_id': self.id,
+                }).sudo().create({
+                    'body': '<p> Для Вас достигнут лимит в ' + str(confirmed_order_lines_total_price) + ' рублей </p>',
+                    'partner_ids': [(4, self.marketplace_seller_id.id)]
+                })
+                composer.send_mail()
+                self.marketplace_seller_id.write({
+                    "total": 0.0
+                })
+                lines.write({
+                    "amount_taken_into_account_user": True
+                })
+            else:
+                self.marketplace_seller_id.write({
+                    "total": confirmed_order_lines_total_price
+                })
