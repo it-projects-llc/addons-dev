795bc3da43f9335cef957756d4d38d96cdbb02ab
ufaks <excellm@yandex.ru>
2019-02-01 09:08:59 +0500

    :arrow_up::one::two: test tour;

--- odoo_backup_sh/static/src/js/tour.js
+++ odoo_backup_sh/static/src/js/tour.js
@@ -11,12 +11,20 @@ odoo.define("odoo_backup_sh.tour", function (require) {
 
     tour.register("odoo_backup_sh_tour", {
             url: "/web",
+            test: true,
         },
         [
+            tour.STEPS.SHOW_APPS_MENU_ITEM,
             {
-                trigger: '.o_app[data-menu-xmlid="odoo_backup_sh.menu_backup_root"], .oe_menu_toggler[data-menu-xmlid="odoo_backup_sh.menu_backup_root"]',
+                trigger: '.o_app[data-menu-xmlid="odoo_backup_sh.menu_backup_root"]',
+                content: _t('Want a better way to <b>manage your databases backups</b>? <i>It starts here.</i>'),
+                position: "right",
+                edition: 'community'
+            }, {
+                trigger: '.o_app[data-menu-xmlid="odoo_backup_sh.menu_backup_root"]',
                 content: _t('Want a better way to <b>manage your databases backups</b>? <i>It starts here.</i>'),
                 position: "bottom",
+                edition: 'enterprise'
             },
             {
                 // This step have been added to make sure that the "Go to Balance" button was shown.
