8209c0862e4f1b1a4ffaa6ce0416cf83e47fc66b
GabbasovDinar <gabbasov@it-projects.info>
2019-02-01 15:40:03 +0500

    :bomb: fix registration error

--- website_itselo/controllers/main.py
+++ website_itselo/controllers/main.py
@@ -2,17 +2,29 @@
 import odoo
 from odoo import http
 from odoo.http import request
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 from odoo.addons.odoo_marketplace.controllers.main import AuthSignupHome
 from odoo.addons.odoo_marketplace.controllers.main import website_marketplace_dashboard
 
 
 class AuthSignupHome(AuthSignupHome):
-
     def do_signup(self, qcontext):
         """ Shared helper that creates a res.partner out of a token """
-        values = {key: qcontext.get(key) for key in (
-        'login', 'name', 'password', 'is_seller', 'is_delivery', 'fleet', 'state_id', 'district_id', 'city_id',
-        'street', 'house', 'flat', 'zip', 'mobile', 'url_handler')}
+        values = {key: qcontext.get(key) for key in ('login', 'name', 'password', 'is_seller', 'is_delivery',
+                                                      'fleet', 'state_id', 'district_id', 'city_id', 'street', 'house',
+                                                      'flat', 'zip', 'mobile', 'url_handler')}
+        if not values:
+            raise UserError(_("The form was not properly filled in."))
+
+        if values.get('password') != qcontext.get('confirm_password'):
+            raise UserError(_("Passwords do not match; please retype them."))
+
+        supported_langs = [lang['code'] for lang in request.env['res.lang'].sudo().search_read([], ['code'])]
+
+        if request.lang in supported_langs:
+            values['lang'] = request.lang
+
         # Доставщик
         if values.get('is_delivery'):
             Fleet = request.env['fleet.vehicle']
@@ -32,36 +44,47 @@ class AuthSignupHome(AuthSignupHome):
             values['fleet_id'] = auto.id
 
         # Адрес
-        state = values.get('state_id')
-        district = values.get('district_id')
-        city = values.get('city_id')
+        country_russia = request.env.ref('base.ru')
+        values['country_id'] = country_russia.id
 
-        state_id = request.env['res.country.state']
-        city_id = request.env['res.city']
-        values['country_id'] = 190
-        search_state = state_id.sudo().search([('code', '=', state)])
-        search_city = city_id.sudo().search([('name', '=', city), ('state_id', '=', search_state.id)]).id
-        # State
-        if search_state:
-            values['state_id'] = search_state.id
-        else:
-            s_id = request.env['res.country.state'].sudo().create({
-                'name': state,
-                'country_id': 190,
-                'code': state,
-            })
-            values['state_id'] = s_id.id
+        state_obj = request.env['res.country.state']
+        state = state_obj.sudo().search([('code', '=', values.get('state_id'))])
 
-        # City
-        if search_city:
-            values['city_id'] = search_city
-        else:
-            c_id = request.env['res.city'].sudo().create({
-                'name': city,
-                'state_id': search_state.id or s_id.id,
+        if not state:
+            state = state_obj.sudo().create({
+                'name': values.get('state_id'),
+                'country_id': country_russia.id,
+                'code': values.get('state_id'),
             })
-            values['city_id'] = c_id.id
-        super(AuthSignupHome, self).do_signup(qcontext)
+
+        values['state_id'] = state.id
+
+        district_obj = request.env['res.state.district']
+        district = district_obj.sudo().search([('name', '=', values.get('district_id')), ('state_id', '=', state.id)])
+
+        city_obj = request.env['res.city']
+        city = city_obj.sudo().search([('name', '=', values.get('city_id')), ('state_id', '=', state.id)])
+
+        if not district:
+            district = district_obj.sudo().create({
+                'name': values.get('district_id'),
+                'state_id': state.id,
+            })
+        else:
+            city = city_obj.sudo().search([('name', '=', values.get('city_id')), ('state_id', '=', state.id),
+                                           ('district_id', '=', district.id)])
+        values['district_id'] = district.id
+
+        if not city:
+            city = city_obj.sudo().create({
+                'name': values.get('city_id'),
+                'state_id': state.id,
+                'district_id': district.id,
+                'country_id': country_russia.id
+            })
+        values['city_id'] = city.id
+        self._signup_with_values(qcontext.get('token'), values)
+        request.env.cr.commit()
 
 
 class WebsiteMarketplaceDashboard(website_marketplace_dashboard):
