94b278f96914186cbdc4131d9f443583fb652012
Ivan Yelizariev <yelizariev@it-projects.info>
2019-02-08 20:07:05 +0500

    :green_heart:

--- payment_pagadito/pagadito.py
+++ payment_pagadito/pagadito.py
@@ -2,8 +2,6 @@
 # Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 import logging
-import urllib
-import urllib2
 import json
 
 _logger = logging.getLogger(__name__)
