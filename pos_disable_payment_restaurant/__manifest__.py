989a9f7060fb0403c544791ef989b1a442a404bc
Ilmir <ilmir.karamov@gmail.com>
2019-02-13 20:51:53 +0500

    :book: pos_disable_payment_restaurant: update for the docs

--- pos_disable_payment_restaurant/__manifest__.py
+++ pos_disable_payment_restaurant/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# Copyright 2018 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Disable options in POS (restaurant extension)""",
     "summary": """Control access to POS restaurant options""",
     "category": "Point of Sale",
-    # "live_test_url": "",
-    "images": [],
+    "live_test_url": "http://apps.it-projects.info/shop/product/pos-disable-payment-restaurant?version=10.0",
+    "images": ['images/pos_disable_payment_restaurant.jpg'],
     "version": "10.0.1.0.0",
     "application": False,
 
@@ -14,7 +15,7 @@
     "support": "pos@it-projects.info",
     "website": "https://it-projects.info/team/GabbasovDinar",
     "license": "LGPL-3",
-    "price": 19.00,
+    "price": 29.00,
     "currency": "EUR",
 
     "depends": [
@@ -38,4 +39,15 @@
 
     "auto_install": False,
     "installable": True,
+
+    "demo_title": "Disable options in POS (restaurant extension)",
+    "demo_addons": [
+    ],
+    "demo_addons_hidden": [
+    ],
+    "demo_url": "pos-disable-payment-restaurant",
+    "demo_summary": "Control access to POS restaurant options",
+    "demo_images": [
+        "images/pos_disable_payment_restaurant.jpg",
+    ]
 }
