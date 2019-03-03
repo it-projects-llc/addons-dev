ab1b8f53f82fb9054bc1c91b37cdf82b67fbdbef
ufaks <excellm@yandex.ru>
2019-03-03 14:27:53 +0500

    :book:

--- odoo_backup_sh/__manifest__.py
+++ odoo_backup_sh/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Backup",
     # "live_test_url": "",
     "images": [],
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Stanislav Krotov",
