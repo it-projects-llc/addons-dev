f15a20eebc3067a0d5e3acfe336310b57e5dc3e9
KolushovAlexandr <kolushov@it-projects.info>
2019-03-13 21:06:58 -0400

    :tada::one::zero: pos_event_registration

---
+++ pos_event_registration/__manifest__.py
@@ -0,0 +1,57 @@
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Register Event Attendees""",
+    "summary": """Process Attendees and Sell Tickets""",
+    "category": "Point of Sale",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=10.0",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Kolushov Alexandr",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/KolushovAlexandr",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "pos_longpolling",
+        "event_sale",
+        "partner_event",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/assets.xml",
+        "views/views.xml",
+        "views/bracelet_views.xml",
+        "views/event_set_views.xml",
+        'security/ir.model.access.csv',
+    ],
+    "demo": [
+    ],
+    "qweb": [
+        "static/src/xml/attendee_screen.xml",
+        "static/src/xml/templates.xml",
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "Register Event Attendees",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "{SHORT module description for REAMDE and manifest}",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
