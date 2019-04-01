354ad810451edd1f95ff43e71931873a215919a1
diga <gabbasov@it-projects.info>
2019-04-01 15:17:59 +0500

    :fire: get Table QR Code for each company

--- pos_wechat_miniprogram/models/pos_restaurant.py
+++ pos_wechat_miniprogram/models/pos_restaurant.py
@@ -12,17 +12,20 @@ class RestaurantTable(models.Model):
     @api.multi
     def get_miniprogram_qr_code(self, access_token=False):
         self.ensure_one()
-        if not self.wechat_miniprogram_qr:
-            param = {
-                'floor_id': self.floor_id,
-                'table_id': self.id
-            }
-            data = {
-                "path": '%s?%s' % ('pages/index/index', werkzeug.urls.url_encode(param)),
-                "width": 430
-            }
-            res = self.env['ir.config_parameter'].sudo().get_qr_code(data, access_token)
-            self.wechat_miniprogram_qr = res
-            return res
+        if self.wechat_miniprogram_qr:
+            return self.wechat_miniprogram_qr
 
-        return self.wechat_miniprogram_qr
+        param = {
+            'company_id': self.env.user.company_id.id,
+            'floor_id': self.floor_id.id,
+            'table_id': self.id
+        }
+        data = {
+            "path": '%s?%s' % ('pages/index/index', werkzeug.urls.url_encode(param)),
+            "width": 430
+        }
+        qr = self.env['ir.config_parameter'].sudo().get_qr_code(data, access_token)
+        self.write({
+            'wechat_miniprogram_qr': qr
+        })
+        return qr
