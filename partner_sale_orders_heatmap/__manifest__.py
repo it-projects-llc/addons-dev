c8d573e9e111c5e52e19006dce59e4b9454e807e
diga <gabbasov@it-projects.info>
2019-04-09 17:45:55 +0500

    :memo: add new module partner sale orders heatmap

---
+++ partner_sale_orders_heatmap/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2019 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Partner Sale Orders HeatMap""",
+    "summary": """Partner Sale Orders HeatMap""",
+    "category": "Extra Tools",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version={ODOO_BRANCH}",
+    "images": [],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/GabbasovDinar",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "sale_management",
+        "web_widget_heatmap"
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/partner_sale_orders_heatmap_view.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
---
+++ partner_sale_orders_heatmap/doc/index.rst
@@ -0,0 +1,8 @@
+=============================
+ Partner Sale Orders HeatMap
+=============================
+
+Installation
+============
+
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
