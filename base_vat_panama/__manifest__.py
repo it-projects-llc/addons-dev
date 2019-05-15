7837259bf44677f1ec60afa5eed8b594b19d4593
KolushovAlexandr <kolushov@it-projects.info>
2019-05-15 18:34:32 +0500

    :zap: ruc,dv fields

--- base_vat_panama/__manifest__.py
+++ base_vat_panama/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
@@ -21,6 +22,7 @@
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
+        "views/partner_views.xml"
     ],
     "demo": [
     ],
--- base_vat_panama/models/models.py
+++ base_vat_panama/models/models.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    ruc = fields.Char('RUC')
+    dv = fields.Char('DV')
 
     def _split_vat(self, vat):
         vat_country = vat[:3].lower()
