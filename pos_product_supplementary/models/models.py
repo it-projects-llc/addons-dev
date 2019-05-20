74d6d9b90fc8aee62c8ed09cc0384a360205c760
KolushovAlexandr <kolushov@it-projects.info>
2019-05-20 18:41:21 +0500

    :tada::one::two: pos_product_supplementary

---
+++ pos_product_supplementary/models/models.py
@@ -0,0 +1,27 @@
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from odoo import models, fields, api
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    supplementary_products = fields.Boolean('Sell Supplementary Products')
+    show_supplementary_products = fields.Boolean('Show Supplementary Products',
+                                                 help='All supplementary products will be displayed in POS'
+                                                      'even if the option `Available in POS` is disabled')
+
+
+class PosOrderLine(models.Model):
+    _inherit = "pos.order.line"
+
+    parented_orderline = fields.Many2one('pos.order.line', string='Sell Supplementary Products')
+    supplementary_line_ids = fields.One2many('pos.order.line', 'parented_orderline', string='Base Product')
+
+    @api.model
+    def create(self, values):
+        if values.get('parented_orderline'):
+            del values['parented_orderline']
+        if values.get('supplementary_line_ids'):
+            del values['supplementary_line_ids']
+        return super(PosOrderLine, self).create(values)
---
+++ pos_product_supplementary/static/src/js/pos.js
@@ -0,0 +1,255 @@
+/* Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
+
+odoo.define('pos_product_supplementary.pos', function (require) {
+    "use strict";
+
+    var models = require('point_of_sale.models');
+    var PosDb = require('point_of_sale.DB');
+    var screens = require('point_of_sale.screens');
+    var PopupWidget = require('point_of_sale.popups');
+    var gui = require('point_of_sale.gui');
+
+    models.load_fields('product.product', ['is_supplementary_product', 'supplementary_product_child_ids', 'available_in_pos']);
+
+    models.load_models({
+        model: 'product.product',
+        fields: function(self) {
+            var product_model = _.find(self.models, function(m){
+                return m.model === 'product.product';
+            });
+            return product_model.fields;
+        },
+//        condition: function(self) {
+//            return self.config.supplementary_products;
+//        },
+        domain: [['is_supplementary_product','=',true]],
+        loaded: function(self, products){
+            if (!products) {
+                return;
+            }
+            var product_model = _.find(self.models, function(m){
+                return m.model === 'product.product';
+            });
+            return product_model.loaded(self,products);
+        },
+    });
+
+    PosDb.include({
+        get_supplementary_products_by_parent_id: function(pid) {
+            var self = this;
+            var product = this.get_product_by_id(pid);
+            var products = [];
+            _.each(product.supplementary_product_child_ids, function(pr_id) {
+                products.push(self.get_product_by_id(pr_id));
+            });
+            return products;
+        },
+    });
+
+    var _super_orderline = models.Orderline.prototype;
+    models.Orderline = models.Orderline.extend({
+
+        get_supplementary_products: function() {
+            return this.pos.db.get_supplementary_products_by_parent_id(this.product.id);
+        },
+
+        get_supplementary_orderlines: function() {
+            var lines = [];
+            var order = this.order;
+            _.each(this.supplementary_line_ids, function(lid){
+                lines.push(order.get_orderline(lid));
+            });
+            return lines;
+        },
+
+        hide_orderline: function() {
+            $(this.node).hide();
+        },
+
+        get_total_for_supplementary_lines: function() {
+            var self = this;
+            var lines = this.supplementary_line_ids;
+            if (!lines.length) {
+                return false;
+            }
+            return _.chain(lines)
+                .map(function(l){
+                    return self.order.get_orderline(l).get_display_price();
+                })
+                .reduce(function(memo, num){
+                    return memo + num;
+                }, 0).value();
+        },
+
+        get_display_price: function(){
+            var result = _super_orderline.get_display_price.apply(this,arguments);
+            if(this.supplementary_line_ids && this.supplementary_line_ids.length){
+                return this.get_total_for_supplementary_lines() + result;
+            }
+            return result;
+        },
+
+        export_as_JSON: function() {
+            var result = _super_orderline.export_as_JSON.apply(this,arguments);
+            if (this.parented_orderline){
+                result.parented_orderline = this.parented_orderline;
+            }
+            if (this.supplementary_line_ids){
+                result.supplementary_line_ids = this.supplementary_line_ids;
+            }
+            return result;
+        },
+
+        init_from_JSON: function(json) {
+            var result = _super_orderline.init_from_JSON.apply(this,arguments);
+            if (json.parented_orderline){
+                this.parented_orderline = json.parented_orderline;
+            }
+            if (json.supplementary_line_ids){
+                this.supplementary_line_ids = json.supplementary_line_ids;
+            }
+            return result;
+        },
+
+    });
+
+    var _super_order = models.Order.prototype;
+    models.Order = models.Order.extend({
+
+        add_product: function(product, options){
+            var self = this;
+            if (!this.pos.config.supplementary_products) {
+                return _super_order.add_product.apply(this,arguments);
+            }
+
+            if (product && product.supplementary_product_child_ids && product.supplementary_product_child_ids.length) {
+                var supplementary_line_ids = [];
+                _.each(this.pos.db.get_supplementary_products_by_parent_id(product.id), function(p) {
+                    self.add_product(p, {
+                        merge: false,
+                    });
+                    supplementary_line_ids.push(self.get_last_orderline().id);
+                });
+                _super_order.add_product.apply(this,arguments);
+                var parented_orderline = self.get_last_orderline();
+                parented_orderline.supplementary_line_ids = supplementary_line_ids;
+                _.each(parented_orderline.get_supplementary_orderlines(), function(l){
+                    l.parented_orderline = parented_orderline.id;
+                });
+                parented_orderline.set_quantity(1);
+                self.pos.gui.screen_instances.products.order_widget.rerender_orderline(parented_orderline);
+            } else {
+                _super_order.add_product.apply(this,arguments);
+            }
+        },
+
+        select_orderline: function(line){
+            if(line && line.parented_orderline){
+                line.hide_orderline();
+                line = this.get_orderline(line.parented_orderline);
+            }
+            return _super_order.select_orderline.apply(this,arguments);
+        },
+    });
+
+    screens.OrderWidget.include({
+
+        rerender_orderline: function(order_line) {
+            this._super(order_line);
+            if (order_line.parented_orderline) {
+                order_line.hide_orderline();
+            }
+            if (order_line.supplementary_line_ids && order_line.supplementary_line_ids.length) {
+                var self = this;
+                _.each(order_line.get_supplementary_orderlines(), function(l){
+                    self.rerender_orderline(l);
+                });
+            }
+        },
+
+        render_orderline: function(orderline){
+            var node = this._super(orderline);
+            if (orderline.parented_orderline) {
+                $(node).hide();
+            }
+            return node;
+        },
+    });
+
+    var SupplementaryButton = screens.ActionButtonWidget.extend({
+        template: 'SupplementaryButton',
+
+        button_click: function(){
+            var self = this;
+            var line_ids = this.pos.get_order().get_selected_orderline().supplementary_line_ids;
+            if (!line_ids || !line_ids.length){
+                return;
+            }
+            var values = [];
+            _.each(line_ids, function(id) {
+                values.push(self.pos.get_order().get_orderline(id));
+            });
+            this.gui.show_popup('supplementary',{
+                'title': 'Supplementary Products',
+                'list': values,
+            });
+        },
+    });
+
+    screens.define_action_button({
+        'name': 'supplementary_button',
+        'widget': SupplementaryButton,
+        'condition': function () {
+            return this.pos.config.supplementary_products;
+        },
+    });
+
+    var SupplementaryPopupWidget = PopupWidget.extend({
+        template: 'SupplementaryPopupWidget',
+
+        show: function(options){
+            var self = this;
+            options = options || {};
+            this._super(options);
+
+            this.list = options.list || [];
+            this.is_selected = options.is_selected || function (item) {
+                return false;
+            };
+            this.renderElement();
+            this.updated_lines = [];
+        },
+
+        click_confirm: function(){
+            this.apply_qty_updates();
+            this.gui.close_popup();
+        },
+
+        apply_qty_updates: function() {
+            var self = this;
+            var lines = this.$el.find('.supplementary_product_line .supp_qty_input input');
+            var order = this.pos.get_order();
+            _.each(lines, function(l){
+                var olid = parseInt(l.getAttribute('olid') || 0);
+                var qty = parseInt(l.value) || 0;
+                order.get_orderline(olid).set_quantity(qty);
+            });
+            var parented_orderline = order.get_orderline(this.list[0].parented_orderline);
+            this.pos.chrome.screens.products.order_widget.rerender_orderline(parented_orderline);
+        },
+    });
+    gui.define_popup({name:'supplementary', widget: SupplementaryPopupWidget});
+
+    screens.ProductListWidget.include({
+        set_product_list: function(product_list){
+            if (!this.pos.config.show_supplementary_products) {
+                product_list = _.filter(product_list, function(p){
+                    return p.available_in_pos;
+                });
+            }
+            return this._super(product_list);
+        },
+    });
+
+});
