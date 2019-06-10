dae419ddd85ed555b7971a70f603b4b34b50dbd4
KolushovAlexandr <kolushov@it-projects.info>
2019-06-10 18:19:49 +0500

    :zap: product_supplementary same fields for prod.prod view

--- product_supplementary/models/product.py
+++ product_supplementary/models/product.py
@@ -7,12 +7,33 @@ class Product(models.Model):
     _inherit = 'product.template'
 
     is_supplementary_product = fields.Boolean('Supplementary Product')
+    supplementary_product_child_ids = fields.Many2many('product.product', 'product_template_supplementary_product',
+                                                       'supplementary_product_child_ids',
+                                                       'supplementary_product_parent_ids',
+                                                       string='Supplementary Products',
+                                                       help='List of Supplementary Products')
+    supplementary_product_parent_ids = fields.Many2many('product.template', 'product_template_supplementary_product',
+                                                        'supplementary_product_parent_ids',
+                                                        'supplementary_product_child_ids',
+                                                        string='Parented Products',
+                                                        help='List of Parented Products')
+
+    @api.onchange('is_supplementary_product')
+    def _onchange_is_supplementary_product(self):
+        self.supplementary_product_parent_ids = False
+        self.supplementary_product_child_ids = False
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    is_supplementary_product = fields.Boolean('Supplementary Product')
     supplementary_product_child_ids = fields.Many2many('product.product', 'product_product_supplementary_product',
                                                        'supplementary_product_child_ids',
                                                        'supplementary_product_parent_ids',
                                                        string='Supplementary Products',
                                                        help='List of Supplementary Products')
-    supplementary_product_parent_ids = fields.Many2many('product.template', 'product_product_supplementary_product',
+    supplementary_product_parent_ids = fields.Many2many('product.product', 'product_product_supplementary_product',
                                                         'supplementary_product_parent_ids',
                                                         'supplementary_product_child_ids',
                                                         string='Parented Products',
