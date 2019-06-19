0718d7477c79c41e0fb3c718780c49a9e5d0467f
KolushovAlexandr <kolushov@it-projects.info>
2019-06-19 18:02:09 +0500

    :bomb: updates

--- pos_mail/models/models.py
+++ pos_mail/models/models.py
@@ -12,34 +12,53 @@ class PosConfig(models.Model):
 
     @api.model
     def send_receipt_via_mail(self, partner_id, body_from_ui, pos_reference):
-
-        partner = self.env['res.partner'].browse(partner_id)
-
-        name = partner.name + ' POS Receipt'
-
         base64_pdf = self.env['ir.actions.report']._run_wkhtmltopdf(
             [body_from_ui.encode()],
             landscape=False,
             specific_paperformat_args={'data-report-margin-top': 10, 'data-report-header-spacing': 10}
         )
-
-        order = self.env['pos.order'].search([('pos_reference', '=', pos_reference)])
-
         attachment = self.env['ir.attachment'].create({
-            'name': name,
-            'datas_fname': name + '.pdf',
+            'name': pos_reference,
+            'datas_fname': pos_reference + '.pdf',
             'type': 'binary',
             'db_datas': base64.encodestring(base64_pdf),
-            'res_model': 'pos.order',
-            'res_id': order and order.id or False,
+            'res_model': 'res.partner',
+            'res_id': partner_id,
         })
 
+        partner = self.env['res.partner'].browse(partner_id)
+        mail_body = 'Receipt from ' + self.env.user.company_id.name + '\n'
+        mail_body += self.env["ir.config_parameter"].sudo().get_param("pos_mail.mail_message", default='')
         # wizard model creation
         composer = self.env['mail.compose.message'].create({
             'partner_ids': [(6, False, [partner.id])],
             'attachment_ids': [(6, False, [attachment.id])],
             'notify': True,
+            'body': mail_body,
         })
-        composer.send_mail()
 
-        return
+        composer.send_mail()
+        return True
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    mail_message = fields.Text('Mail Message Template')
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        config_parameters = self.env["ir.config_parameter"].sudo()
+        for record in self:
+            config_parameters.sudo().set_param("pos_mail.mail_message", record.mail_message)
+
+    @api.multi
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        config_parameters = self.env["ir.config_parameter"].sudo()
+        mail_message = config_parameters.sudo().get_param("pos_mail.mail_message", default='')
+        res.update(
+            mail_message=mail_message,
+        )
+        return res
--- pos_mail/static/src/js/pos.js
+++ pos_mail/static/src/js/pos.js
@@ -10,20 +10,43 @@ odoo.define('pos_mail.pos', function (require) {
     var _t = core._t;
 
     screens.ReceiptScreenWidget.include({
+        show: function() {
+            this._super();
+            this.able_mail_button();
+        },
+
         renderElement: function() {
             this._super();
             var self = this;
             if (this.pos.config.send_receipt_by_mail) {
                 this.$('.button.mail_receipt').click(function(){
+                    self.disable_mail_button();
                     self.mail_receipt_action();
                 });
             }
         },
 
+        able_mail_button: function() {
+            return this.$('.button.mail_receipt').removeClass('disable');
+        },
+
+        disable_mail_button: function() {
+            return this.able_mail_button().addClass('disable');
+        },
+
         mail_receipt_action: function() {
+            var self = this;
             var partner = this.pos.get_order().get_client();
             if (partner) {
-                this.send_mail_receipt(partner.id);
+                return this.send_mail_receipt(partner.id).done(function(res){
+                    console.log("Mail's sent");
+                }).fail(function(error){
+                    self.gui.show_popup('error-traceback',{
+                        'title': error.message || _t("Server Error"),
+                        'body': error.data.debug || _t('The server encountered an error while trying to send the mail.'),
+                    });
+                    self.able_mail_button();
+                });
             } else {
                 this.set_mail_customer = true;
                 this.$el.zIndex(-6);
