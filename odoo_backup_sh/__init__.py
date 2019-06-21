81cb36802d69d306e8bc3a05ae1584716cb8ed41
diga <gabbasov@it-projects.info>
2019-06-21 17:08:46 +0500

    :memo: help

--- odoo_backup_sh/__init__.py
+++ odoo_backup_sh/__init__.py
@@ -6,6 +6,10 @@ from odoo import api, SUPERUSER_ID
 
 
 def uninstall_hook(cr, registry):
+    # During the unistall of the module, we have the error:
+    # 'update or delete on table "ir_act_server" violates foreign key constraint
+    # "ir_cron_ir_actions_server_id_fkey" on table "ir_cron"'. Due to this error the "odoo_backup_sh.config" table
+    # will not be deleted. Below the code allows you to delete all entries related to "ir_cron".
     env = api.Environment(cr, SUPERUSER_ID, {})
     backup_crons = env['odoo_backup_sh.config.cron'].search([('model_name', '=', 'odoo_backup_sh.config')])
     if backup_crons:
