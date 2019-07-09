d261053ac47f609380a64a9cb761602aaecd00cd
KolushovAlexandr <kolushov@it-projects.info>
2019-07-09 17:04:30 +0500

    :bomb: updated js

--- fleet_parse_from_xml/static/src/js/view_controllers.js
+++ fleet_parse_from_xml/static/src/js/view_controllers.js
@@ -2,6 +2,7 @@ odoo.define('fleet_parse_from_xml.ListController', function (require) {
 "use strict";
 
 var ListController = require('web.ListController');
+var FormController = require('web.FormController');
 
 ListController.include({
     _onCreateRecord: function (event) {
@@ -27,11 +28,38 @@ ListController.include({
             });
             return {
                 'type': 'ir.actions.client',
-//                'tag': 'reload',
             }
         }
     },
+});
 
+FormController.include({
+    _onCreate: function (event) {
+        if (this.modelName !== 'driving.data') {
+            this._super(event);
+        } else {
+            this.do_action({
+                type: "ir.actions.act_window",
+                name: "Create New Driving Data",
+                res_model: "driving.data.wizard",
+                views: [[false,'form']],
+                target: 'new',
+                view_type : 'form',
+                view_mode : 'form',
+                flags: {
+                    'form': {
+                        'action_buttons': true,
+                        'options': {
+                            'mode': 'edit'
+                        }
+                    }
+                }
+            });
+            return {
+                'type': 'ir.actions.client',
+            }
+        }
+    },
 });
 
 });
