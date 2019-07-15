25fc389d0542c6c180992e0ee4a46e62a1b551c2
ommo73 <rafailov@it-projects.info>
2019-07-15 16:09:42 +0500

    :arrow_up::one::two:

--- fsm_custom/models/fields_project_task.py
+++ fsm_custom/models/fields_project_task.py
@@ -3,6 +3,7 @@ from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError,Warning
 import base64
 import logging
+import wdb
 logger = logging.getLogger(__name__)
 
 
@@ -640,24 +641,25 @@ class ModifyLine(models.Model):
     order_id = fields.Many2one(
         comodel_name='sale.order',
         string='Order Reference',
-        required=True,
+        required=False,
         help='This field is a sale order reference field')
 
-    # @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
-    # def _compute_amount(self):
-    #     """
-    #     Compute the amounts of the SO line.
-    #     """
-    #     for line in self:
-    #         self.price_unit = line.product_id.list_price
-    #         line.price_unit = line.product_id.list_price
-    #         price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-    #         taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-    #         line.update({
-    #             'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-    #             'price_total': taxes['total_included'],
-    #             'price_subtotal': taxes['total_excluded'],
-    #         })
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        wdb.set_trace()
+        for line in self:
+            self.price_unit = line.product_id.list_price
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
+                                            product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
 
 
 class JobList(models.Model):
