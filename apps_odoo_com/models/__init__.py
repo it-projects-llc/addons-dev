f7944123bd045535e5061b51cd11a5e22db6cba7
Eugene Molotov <molotov@it-projects.info>
2019-08-07 14:00:55 +0500

    :tada: apps_odoo_com: copy from 8.0

---
+++ apps_odoo_com/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 IT-Projects LLC (<https://it-projects.info>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import res_config
+from . import stats
