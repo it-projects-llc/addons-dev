9478fbeda8688b564a117b5e843f1224c254640a
fedoranvar <jakildebek@gmail.com>
2019-10-30 14:10:19 +0500

    :ambulance: Popup alert-message for exceding available quantity of product by orderlines

--- pos_product_available_negative/static/src/js/pos.js
+++ pos_product_available_negative/static/src/js/pos.js
@@ -1,5 +1,6 @@
 /*  Copyright 2016 Stanislav Krotov <https://it-projects.info/team/ufaks>
     Copyright 2018-2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+    Copyright 2019 Anvar Kildebekov <https://it-projects.info/team/fedoranvar>
     License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html). */
 odoo.define('pos_product_available_negative.pos', function (require) {
     "use strict";
@@ -19,20 +20,28 @@ odoo.define('pos_product_available_negative.pos', function (require) {
             var order = this.pos.get_order();
             var orderlines = order.get_orderlines();
             var has_negative_product = false;
+            var unique_products = {};
             for (var i = 0; i < orderlines.length; i++) {
                 var line = orderlines[i];
-                if (line.product.type === 'product' && line.product.qty_available < line.quantity) {
-                    has_negative_product = true;
-                    self.gui.sudo_custom({
-                        'title': _t('Order has out-of-stock product and must be approved by supervisor'),
-                        'special_group': this.pos.config.negative_order_group_id[0],
-                        'do_not_change_cashier': true,
-                        'product-imgarguments': {'ask_untill_correct': true},
-                    }).done(function(user){
-                        order.negative_stock_user_id = user;
-                        _super.call(self, force_validation);
-                    });
-                    break;
+                if (line.product.type === 'product') {
+                    if (line.product.id in unique_products) {
+                        unique_products[line.product.id] += line.quantity;
+                    } else {
+                        unique_products[line.product.id] = line.quantity;
+                    }
+                    if (line.product.qty_available < unique_products[line.product.id]) {
+                        has_negative_product = true;
+                        self.gui.sudo_custom({
+                            'title': _t('Order has out-of-stock product and must be approved by supervisor'),
+                            'special_group': this.pos.config.negative_order_group_id[0],
+                            'do_not_change_cashier': true,
+                            'product-imgarguments': {'ask_untill_correct': true},
+                        }).done(function(user){
+                            order.negative_stock_user_id = user;
+                            _super.call(self, force_validation);
+                        });
+                        break;
+                    }
                 }
             }
             if (!has_negative_product) {
@@ -52,6 +61,23 @@ odoo.define('pos_product_available_negative.pos', function (require) {
         }
     });
 
+    function check_qty_of_orderlines(self, product, quantity_to_add) {
+        var orderlines = self.pos.get_order().get_orderlines();
+        var product_quantity_to_buy = 0;
+        for (var i = 0; i < orderlines.length; i++) {
+            var line = orderlines[i];
+            if (self.pos.db.get_product_by_id(line.product.id).id === product.id) {
+                product_quantity_to_buy += line.quantity;
+            }
+        }
+        if ((product_quantity_to_buy + quantity_to_add)>product.qty_available) {
+            self.gui.show_popup('alert',{
+                'title': _t("Quantity exceeded available"),
+                'body': _t("You've entered quantity of product that exceeded available in stock"),
+            });
+        }
+    }
+
     screens.ProductListWidget.include({
         init: function(parent, options) {
             var self = this;
@@ -59,20 +85,46 @@ odoo.define('pos_product_available_negative.pos', function (require) {
             if (!this.pos.config.negative_order_warning) {
                 return;
             }
-
             var click_product_handler_super = this.click_product_handler;
             this.click_product_handler = function(){
                 var product = self.pos.db.get_product_by_id(this.dataset.productId);
-                if (product.type === 'product' && product.qty_available <= 0) {
-                    var that = this;
-                    return self.gui.show_popup('alert',{
-                            'title': _t('The product is out of stock'),
-                            'body': _t("It's unavailable to add the product"),
-                        });
+                if (product.type === 'product') {
+                    if (product.qty_available <= 0) {
+                        var that = this;
+                        return self.gui.show_popup('alert',{
+                                'title': _t('The product is out of stock'),
+                                'body': _t("It's unavailable to add the product"),
+                            });
+                    }
+                    check_qty_of_orderlines(self, product, 1);    
                 }
                 _.bind(click_product_handler_super, this)();
             };
         },
     });
+
+    screens.OrderWidget.include({
+        set_value: function(val) {
+            if (!this.pos.config.negative_order_warning) {
+                this._super();
+                return;
+            }
+            var self = this;
+            var order = this.pos.get_order();
+            if (!order.get_selected_orderline()) {
+                return;
+            }
+            var mode = this.numpad_state.get('mode');
+            if( mode !== 'quantity'){
+                return;
+            }
+            var selected_orderline = order.get_selected_orderline();
+            var product = self.pos.db.get_product_by_id(selected_orderline.product.id);
+            if (product.type === 'product'){
+                check_qty_of_orderlines(self, product, parseInt(val));
+            }
+            selected_orderline.set_quantity(val);
+        }
+     });
 
 });
