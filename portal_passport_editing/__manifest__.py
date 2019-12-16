2a73d82c6bb49778f36848caaf913324ee1e7cec
KolushovAlexandr <alexandr.frrost@gmail.com>
2019-12-16 16:21:07 +0500

    :tada::one::zero: portal_passport_editing

---
+++ portal_passport_editing/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Passport Portal Editing""",
+    "summary": """Allows portal users change passport information in portal""",
+    "category": "Marketing",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/portal-event-tickets?version=10.0",
+    "images": ["images/banner.jpg"],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Kolushov Alexandr",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "website_event_attendee_fields_custom",
+        "website_portal",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/website_portal_templates.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
---
+++ portal_passport_editing/controllers/controllers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.addons.website_portal.controllers.main import website_account
+
+
+class website_account_passport(website_account):
+
+    OPTIONAL_BILLING_FIELDS = website_account.OPTIONAL_BILLING_FIELDS + ["passport"]
