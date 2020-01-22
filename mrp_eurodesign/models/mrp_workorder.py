354085eede38f2475a0bde6f4e46944a9c423dd1
Denis Mudarisov <mudarisov@it-projects.info>
2020-01-22 20:15:42 +0500

    :bath: add case when the quantity produced more

--- mrp_eurodesign/models/mrp_workorder.py
+++ mrp_eurodesign/models/mrp_workorder.py
@@ -8,6 +8,14 @@ class MrpWorkorder(models.Model):
 
     _inherit = 'mrp.workorder'
 
+    def do_finish(self):
+        self.ensure_one()
+        produced = self.qty_produced + self.qty_producing
+        if produced > self.qty_production:
+            self.production_id.product_qty = produced
+        res = super(MrpWorkorder, self).do_finish()
+        return res
+
     def button_force_workorder(self):
         self.ensure_one()
         self.production_id.product_qty = self.qty_produced + self.qty_producing
