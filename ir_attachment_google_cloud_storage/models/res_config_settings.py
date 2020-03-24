537a7738ea247564f35c74a1a98af3cd2fe39cd0
Eugene Molotov <molotov@it-projects.info>
2020-03-24 16:00:18 +0500

    added fix

--- ir_attachment_google_cloud_storage/models/res_config_settings.py
+++ ir_attachment_google_cloud_storage/models/res_config_settings.py
@@ -5,10 +5,21 @@ from odoo import api, fields, models
 from google.oauth2 import service_account
 from google.cloud import storage
 import json
+import os
+
+GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+GCS_BUCKETNAME = os.environ.get("GCS_BUCKETNAME")
 
 class ResConfigSettings(models.TransientModel):
 
     _inherit = "res.config.settings"
+
+    def _compute_is_in_env(self):
+        self.is_google_cloud_storage_credentials_in_env = bool(GOOGLE_APPLICATION_CREDENTIALS)
+        self.is_google_cloud_storage_bucket_in_env = bool(GCS_BUCKETNAME)
+
+    is_google_cloud_storage_credentials_in_env = fields.Boolean(compute=_compute_is_in_env, store=False)
+    is_google_cloud_storage_bucket_in_env = fields.Boolean(compute=_compute_is_in_env, store=False)
 
     google_cloud_storage_credentials = fields.Char(
         string="Google Application Credentials (for Google Cloud Storage)",
@@ -21,8 +32,14 @@ class ResConfigSettings(models.TransientModel):
 
 
     def get_google_cloud_storage_client(self):
-        icp = self.env["ir.config_parameter"].sudo()
-        credentials = icp.get_param("google_cloud_storage.credentials")
+        credentials = None
+
+        if GOOGLE_APPLICATION_CREDENTIALS:
+            with open(GOOGLE_APPLICATION_CREDENTIALS, "r") as f:
+                credentials = f.read()
+        else:
+            icp = self.env["ir.config_parameter"].sudo()
+            credentials = icp.get_param("google_cloud_storage.credentials")
 
         if not credentials:
             raise Exception("No Google Cloud Storage credendtials given")
@@ -36,9 +53,13 @@ class ResConfigSettings(models.TransientModel):
 
     def get_google_cloud_storage_bucket(self):
         client = self.get_google_cloud_storage_client()
+        bucket = None
 
-        icp = self.env["ir.config_parameter"].sudo()
-        bucket = icp.get_param("google_cloud_storage.bucket")
+        if GCS_BUCKETNAME:
+            bucket = GCS_BUCKETNAME
+        else:
+            icp = self.env["ir.config_parameter"].sudo()
+            bucket = icp.get_param("google_cloud_storage.bucket")
 
         if not bucket:
             raise Exception("No Google Cloud Storage bucket given")
